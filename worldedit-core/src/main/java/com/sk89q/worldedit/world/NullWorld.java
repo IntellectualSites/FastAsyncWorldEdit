1d3cf75d06a2f09e2e5f0cc5471738770019cb2b
Jesse Boyd <jessepaleg@gmail.com>
2018-08-15 14:13:41 +1000

    *Fix merge

--- worldedit-core/src/main/java/com/sk89q/worldedit/world/NullWorld.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/world/NullWorld.java
@@ -19,40 +19,22 @@
 
 package com.sk89q.worldedit.world;
 
-import com.sk89q.worldedit.EditSession;
-import com.sk89q.worldedit.MaxChangedBlocksException;
-import com.sk89q.worldedit.Vector;
-import com.sk89q.worldedit.Vector2D;
-import com.sk89q.worldedit.WorldEditException;
-<<<<<<< HEAD
-import com.sk89q.worldedit.blocks.BaseBlock;
-import com.sk89q.worldedit.world.block.BlockState;
-=======
->>>>>>> refs/remotes/sk89q/master
+import com.sk89q.worldedit.*;
 import com.sk89q.worldedit.blocks.BaseItemStack;
-import com.sk89q.worldedit.blocks.LazyBlock;
-import com.sk89q.worldedit.world.block.BlockStateHolder;
-import com.sk89q.worldedit.world.block.BlockState;
-import com.sk89q.worldedit.world.block.BlockTypes;
 import com.sk89q.worldedit.entity.BaseEntity;
 import com.sk89q.worldedit.entity.Entity;
 import com.sk89q.worldedit.regions.Region;
 import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldedit.util.TreeGenerator.TreeType;
 import com.sk89q.worldedit.world.biome.BaseBiome;
-<<<<<<< HEAD
-=======
-import com.sk89q.worldedit.world.block.BaseBlock;
 import com.sk89q.worldedit.world.block.BlockState;
 import com.sk89q.worldedit.world.block.BlockStateHolder;
 import com.sk89q.worldedit.world.block.BlockTypes;
->>>>>>> refs/remotes/sk89q/master
 import com.sk89q.worldedit.world.weather.WeatherType;
 
+import javax.annotation.Nullable;
 import java.util.Collections;
 import java.util.List;
-
-import javax.annotation.Nullable;
 
 /**
  * A null implementation of {@link World} that drops all changes and
