EngineHub/WorldEdit

Start work on modularising masks and patterns

worldedit-core/src/main/java/com/sk89q/worldedit/extension/factory/parser/pattern/ClipboardPatternParser.java
@@ -1,0 +1,64 @@
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.extension.factory.parser.pattern;
+
+import com.google.common.collect.Lists;
+import com.sk89q.worldedit.EmptyClipboardException;
+import com.sk89q.worldedit.LocalSession;
+import com.sk89q.worldedit.WorldEdit;
+import com.sk89q.worldedit.extension.input.InputParseException;
+import com.sk89q.worldedit.extension.input.ParserContext;
+import com.sk89q.worldedit.extent.clipboard.Clipboard;
+import com.sk89q.worldedit.function.pattern.ClipboardPattern;
+import com.sk89q.worldedit.function.pattern.Pattern;
+import com.sk89q.worldedit.internal.registry.SimpleInputParser;
+import com.sk89q.worldedit.session.ClipboardHolder;
+
+import java.util.List;
+
+public class ClipboardPatternParser extends SimpleInputParser<Pattern> {
+
+    public ClipboardPatternParser(WorldEdit worldEdit) {
+        super(worldEdit);
+    }
+
+    @Override
+    public List<String> getMatchedAliases() {
+        return Lists.newArrayList("#clipboard", "#copy");
+    }
+
+    @Override
+    public Pattern parseFromSimpleInput(String input, ParserContext context) throws InputParseException {
+        LocalSession session = context.requireSession();
+
+        if (session != null) {
+            try {
+                ClipboardHolder holder = session.getClipboard();
+                Clipboard clipboard = holder.getClipboard();
+                return new ClipboardPattern(clipboard);
+            } catch (EmptyClipboardException e) {
+                throw new InputParseException("To use #clipboard, please first copy something to your clipboard");
+            }
+        } else {
+            throw new InputParseException("No session is available, so no clipboard is available");
+        }
+    }
+
+}

IntellectualSites/FastAsyncWorldEdit

worldedit-core/src/main/java/com/sk89q/worldedit/extension/factory/parser/pattern/ClipboardPatternParser.java
@@ -1,4 +1,23 @@
-package com.sk89q.worldedit.extension.factory;
+/*
+ * WorldEdit, a Minecraft world manipulation toolkit
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldEdit team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sk89q.worldedit.extension.factory.parser.pattern;
 
 import com.boydti.fawe.command.FaweParser;
 import com.boydti.fawe.command.SuggestInputParseException;
@@ -7,6 +26,10 @@ import com.boydti.fawe.object.random.TrueRandom;
 import com.boydti.fawe.util.StringMan;
 import com.sk89q.minecraft.util.commands.CommandException;
 import com.sk89q.minecraft.util.commands.CommandLocals;
+
+import com.google.common.collect.Lists;
+import com.sk89q.worldedit.EmptyClipboardException;
+import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.command.PatternCommands;
 import com.sk89q.worldedit.extension.input.InputParseException;
@@ -30,10 +53,10 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class HashTagPatternParser extends FaweParser<Pattern> {
+public class ClipboardPatternParser extends FaweParser<Pattern> {
     private final Dispatcher dispatcher;
 
-    public HashTagPatternParser(WorldEdit worldEdit) {
+    public ClipboardPatternParser(WorldEdit worldEdit) {
         super(worldEdit);
         this.dispatcher = new SimpleDispatcher();
         this.register(new PatternCommands(worldEdit));
@@ -179,8 +202,6 @@ public class HashTagPatternParser extends FaweParser<Pattern> {
                 random.add(patterns.get(i), chances.get(i));
             }
             return random;
-        }
+            }
     }
-
-
 }