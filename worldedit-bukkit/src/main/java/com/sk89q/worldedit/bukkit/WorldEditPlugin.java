fee57017cd8f5a2e88f846259bb591702f9cdca1
NotMyFault <mc.cache@web.de>
2019-07-11 22:50:28 +0200

    Bump dummy version to 1.14

--- worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
+++ worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
@@ -276,7 +276,7 @@ public class WorldEditPlugin extends JavaPlugin { //implements TabCompleter
         }
         File pluginsFolder = MainUtil.getJarFile().getParentFile();
         for (File file : pluginsFolder.listFiles()) {
-            if (file.length() == 2016) return;
+            if (file.length() == 2009) return;
         }
         Plugin plugin = Bukkit.getPluginManager().getPlugin("FastAsyncWorldEdit");
         File dummy = MainUtil.copyFile(MainUtil.getJarFile(), "DummyFawe.src", pluginsFolder, "DummyFawe.jar");
