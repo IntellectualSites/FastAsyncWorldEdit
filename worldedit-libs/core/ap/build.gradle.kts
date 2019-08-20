6998c2d230b326cdafc6e0c7eea89dc4d2c931f0
MattBDev <4009945+MattBDev@users.noreply.github.com>
2019-08-20 16:03:50 -0400

    fix compile

--- worldedit-libs/core/ap/build.gradle.kts
+++ worldedit-libs/core/ap/build.gradle.kts
@@ -1,9 +1,11 @@
 applyLibrariesConfiguration()
 
 dependencies {
+/*
     "shade"("FAWE-Piston:core/build/libs/core-${Versions.PISTON}:lastSuccessfulBuild@jar")
     "shade"("FAWE-Piston:core-ap/runtime/build/libs/runtime-${Versions.PISTON}:lastSuccessfulBuild@jar")
     "shade"("FAWE-Piston:default-impl/build/libs/default-impl-${Versions.PISTON}:lastSuccessfulBuild@jar")
+*/
     "shade"("org.enginehub.piston.core-ap:annotations:${Versions.PISTON}")
     "shade"("org.enginehub.piston.core-ap:processor:${Versions.PISTON}")
 }
