c36db8de3765987501b1d7b16003e0c30ad2b6a5
MattBDev <4009945+MattBDev@users.noreply.github.com>
2019-08-22 21:10:31 -0400

    Attempt 2

--- worldedit-core/build.gradle.kts
+++ worldedit-core/build.gradle.kts
@@ -59,9 +59,9 @@ sourceSets {
 
 tasks.named<Copy>("processResources") {
     filesMatching("fawe.properties") {
-        expand("version" to "$version")
-        expand("commit" to "${rootProject.ext["revision"]}")
-        expand("date" to "${rootProject.ext["date"]}")
+        expand("version" to "$version",
+                "commit" to "${rootProject.ext["revision"]}",
+                "date" to "${rootProject.ext["date"]}")
     }
 }
 tasks.named<ShadowJar>("shadowJar") {
