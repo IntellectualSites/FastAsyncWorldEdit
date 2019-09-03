c65e06cb1ba8128107f5db946901ee31230c62da
MattBDev <4009945+MattBDev@users.noreply.github.com>
2019-09-03 15:40:59 -0400

    Removed tool command

--- worldedit-core/src/main/java/com/sk89q/worldedit/command/ToolCommands.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/command/ToolCommands.java
@@ -76,15 +76,16 @@ public class ToolCommands {
         this.we = we;
     }
 
-    @Command(
-        name = "none",
-        desc = "Unbind a bound tool from your current item"
-    )
-    public void none(Player player, LocalSession session) throws WorldEditException {
+//    @Command(
+//        name = "none",
+//        desc = "Unbind a bound tool from your current item"
+//    )
+//    public void none(Player player, LocalSession session) throws WorldEditException {
+//
+//        session.setTool(player.getItemInHand(HandSide.MAIN_HAND).getType(), null);
+//        player.print("Tool unbound from your current item.");
+//    }
 
-        session.setTool(player.getItemInHand(HandSide.MAIN_HAND).getType(), null);
-        player.print("Tool unbound from your current item.");
-    }
     @Command(
         name = "/selwand",
         aliases = "selwand",
