4dd7036bd4b1094abcffdd181435c6f71ea03d46
NotMyFault <mc.cache@web.de>
2019-11-23 15:09:06 +0100

    Fixes #259 debugpaste not working

--- worldedit-core/src/main/java/com/boydti/fawe/util/IncendoPaster.java
+++ worldedit-core/src/main/java/com/boydti/fawe/util/IncendoPaster.java
@@ -243,7 +243,7 @@ public final class IncendoPaster implements Paster {
 
         incendoPaster.addFile(new PasteFile("config.yml", readFile(new File(Fawe.imp().getDirectory(), "config.yml"))));
         incendoPaster.addFile(new PasteFile("config-legacy.yml", readFile(new File(Fawe.imp().getDirectory(), "config-legacy.yml"))));
-        incendoPaster.addFile(new PasteFile("message.yml", readFile(new File(Fawe.imp().getDirectory(), "message.yml"))));
+//        incendoPaster.addFile(new PasteFile("message.yml", readFile(new File(Fawe.imp().getDirectory(), "message.yml"))));
 //        incendoPaster.addFile(new PasteFile("commands.yml", readFile(new File(Fawe.imp().getDirectory(), "commands.yml"))));
 
         final String rawResponse;
