0a0b41bf0b6eb9f86868fad966361730e5ee31f6
NotMyFault <mc.cache@web.de>
2020-03-10 00:06:26 +0100

    Update discord links

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ logger.lifecycle("""
  1) Read COMPILING.md if you haven't yet
  2) Try running 'build' in a separate Gradle run
  3) Use gradlew and not gradle
- 4) If you still need help, ask on Discord! https://discord.gg/ngZCzbU
+ 4) If you still need help, ask on Discord! https://discord.gg/KxkjDVg
 
  Output files will be in [subproject]/build/libs
 *******************************************
