9869cf09c6d79bc9f99e2a470dfd2a16f078e540
dordsor21 <dordsor21@gmail.com>
2020-04-30 15:14:59 +0100

    Apply fda7d00747abe97d7891b80ed8bb88d97e1c70d1 to other versions so numpties that use old versions for no reason stop complaining too

--- worldedit-bukkit/src/main/java/com/boydti/fawe/bukkit/adapter/mc1_15/BukkitGetBlocks_1_15.java
+++ worldedit-bukkit/src/main/java/com/boydti/fawe/bukkit/adapter/mc1_15/BukkitGetBlocks_1_15.java
@@ -214,10 +214,9 @@ public class BukkitGetBlocks_1_15 extends CharGetBlocks {
             if (this.sections == null) {
                 this.sections = sections.clone();
             }
-            //TODO: Understand why this causes #329, what the purpose of this is, and what may or may not break after commenting this out.
-//            if (this.sections[layer] != section) {
-//                this.sections[layer] = section;
-//            }
+            if (this.sections[layer] != section) {
+                this.sections[layer] = section;
+            }
             this.blocks[layer] = arr;
         }
     }
@@ -287,19 +286,17 @@ public class BukkitGetBlocks_1_15 extends CharGetBlocks {
                     synchronized (this) {
                         synchronized (lock) {
                             lock.untilFree();
-                            ChunkSection getSection;
                             if (this.nmsChunk != nmsChunk) {
                                 this.nmsChunk = nmsChunk;
                                 this.sections = null;
                                 this.reset();
-                            } else {
-                                getSection = this.getSections()[layer];
-                                if (getSection != existingSection) {
-                                    this.sections[layer] = existingSection;
-                                    this.reset();
-                                } else if (lock.isModified()) {
-                                    this.reset(layer);
-                                }
+                            } else if (existingSection != getSections()[layer]) {
+                                this.sections[layer] = existingSection;
+                                this.reset();
+                            } else if (!Arrays.equals(update(layer, new char[4096]), load(layer))) {
+                                this.reset(layer);
+                            } else if (lock.isModified()) {
+                                this.reset(layer);
                             }
                             newSection = BukkitAdapter_1_15.newChunkSection(layer, this::load, setArr);
                             if (!BukkitAdapter_1_15.setSectionAtomic(sections, existingSection, newSection, layer)) {
