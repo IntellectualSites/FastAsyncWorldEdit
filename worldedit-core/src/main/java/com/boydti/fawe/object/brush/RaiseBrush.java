ebca08b02a96b918fc076348df805f61db200d79
IronApollo <zknownone@gmail.com>
2020-09-08 19:17:28 -0400

    Fix erosion and pull brushes
    Fixes #559

--- worldedit-core/src/main/java/com/boydti/fawe/object/brush/RaiseBrush.java
+++ worldedit-core/src/main/java/com/boydti/fawe/object/brush/RaiseBrush.java
@@ -5,6 +5,6 @@ public class RaiseBrush extends ErodeBrush {
         this(6, 0, 1, 1);
     }
     public RaiseBrush(int erodeFaces, int erodeRec, int fillFaces, int fillRec) {
-        super(2, 1, 5, 1);
+        super(erodeFaces, erodeRec, fillFaces, fillRec);
     }
 }
