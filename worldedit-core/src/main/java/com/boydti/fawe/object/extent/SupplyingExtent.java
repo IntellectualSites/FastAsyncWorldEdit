0685881f64296956e6f0f4c86d0b4792a544adcd
Hannes Greule <SirYwell@users.noreply.github.com>
2020-09-14 12:20:26 +0200

    Implement SupplyingExtent (#613)

---
+++ worldedit-core/src/main/java/com/boydti/fawe/object/extent/SupplyingExtent.java
@@ -0,0 +1,24 @@
+package com.boydti.fawe.object.extent;
+
+import com.sk89q.worldedit.extent.Extent;
+import com.sk89q.worldedit.extent.PassthroughExtent;
+
+import java.util.function.Supplier;
+
+/**
+ * An extent that delegates actions to another extent that may change at any time.
+ */
+public class SupplyingExtent extends PassthroughExtent {
+
+    private final Supplier<Extent> extentSupplier;
+
+    public SupplyingExtent(Supplier<Extent> extentSupplier) {
+        super(extentSupplier.get());
+        this.extentSupplier = extentSupplier;
+    }
+
+    @Override
+    public Extent getExtent() {
+        return this.extentSupplier.get();
+    }
+}
