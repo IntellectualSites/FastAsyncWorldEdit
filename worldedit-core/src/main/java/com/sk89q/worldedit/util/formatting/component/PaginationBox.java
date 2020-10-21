EngineHub/WorldEdit

Names via Translation (#1268)
* Deprecate BiomeRegistry, etc.

* Update some libraries, e.g. text

* Move to new translation renderer

* Revert "Deprecate BiomeRegistry, etc."

This reverts commit 59a5d6c92aec52739a8dc68ac3d23898af7593dd.

This was not a good idea for potential mod shenanigans.

* Move BiomeData#getName to BiomeRegistry, use i18n

* Use getRichName instead of getName

* Implement getRichName for NullBiomeRegistry

* Add getRichName for blocks

* Relocate net.kyori.minecraft

* Update adapters for getRichBlockName

* Add getRichName for items

* Update adapters for getRichItemName

* Update adapters JAR for merge

worldedit-core/src/main/java/com/sk89q/worldedit/util/formatting/component/PaginationBox.java
@@ -27,6 +27,7 @@ import com.sk89q.worldedit.util.formatting.text.format.TextColor;
 
 import javax.annotation.Nullable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public abstract class PaginationBox extends MessageBox {
 
@@ -129,20 +130,26 @@ public abstract class PaginationBox extends MessageBox {
     }
 
     public static PaginationBox fromStrings(String header, @Nullable String pageCommand, List<String> lines) {
+        return fromComponents(header, pageCommand, lines.stream()
+            .map(TextComponent::of)
+            .collect(Collectors.toList()));
+    }
+
+    public static PaginationBox fromComponents(String header, @Nullable String pageCommand, List<Component> lines) {
         return new ListPaginationBox(header, pageCommand, lines);
     }
 
     private static class ListPaginationBox extends PaginationBox {
-        private final List<String> lines;
+        private final List<Component> lines;
 
-        ListPaginationBox(String header, String pageCommand, List<String> lines) {
+        ListPaginationBox(String header, String pageCommand, List<Component> lines) {
             super(header, pageCommand);
             this.lines = lines;
         }
 
         @Override
         public Component getComponent(int number) {
-            return TextComponent.of(lines.get(number));
+            return lines.get(number);
         }
 
         @Override

IntellectualSites/FastAsyncWorldEdit

(cherry picked from commit cfd26253b6fb59ff6c65a0157a6780be7db4ea5a)

worldedit-core/src/main/java/com/sk89q/worldedit/util/formatting/component/PaginationBox.java
@@ -21,17 +21,19 @@ package com.sk89q.worldedit.util.formatting.component;
 
 import com.google.common.base.Function;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.ImmutableList;
+import com.sk89q.worldedit.history.changeset.ChangeSet;
 import com.sk89q.worldedit.util.formatting.text.Component;
 import com.sk89q.worldedit.util.formatting.text.TextComponent;
 import com.sk89q.worldedit.util.formatting.text.event.ClickEvent;
 import com.sk89q.worldedit.util.formatting.text.event.HoverEvent;
 import com.sk89q.worldedit.util.formatting.text.format.TextColor;
 
+import javax.annotation.Nullable;
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 import java.util.function.Supplier;
-import javax.annotation.Nullable;
+import java.util.stream.Collectors;
 
 public abstract class PaginationBox extends MessageBox {
 
@@ -145,46 +147,27 @@ public abstract class PaginationBox extends MessageBox {
         return new ListPaginationBox(header, pageCommand, lines);
     }
 
-    public static PaginationBox fromStrings(String header, @Nullable String pageCommand, List<String> lines) {
-        return fromStrings(header, pageCommand, (Collection) lines);
+    public static PaginationBox fromStrings(String header, @Nullable String pageCommand, Collection<String> lines) {
+        return fromComponents(header, pageCommand, lines.stream()
+            .map(TextComponent::of)
+            .collect(Collectors.toList()));
     }
 
-    public static class ListPaginationBox extends PaginationBox {
-        private final Collection lines;
-        private int iterIndex;
-        private Iterator iterator;
+    public static PaginationBox fromComponents(String header, @Nullable String pageCommand, Collection<Component> lines) {
+        return new ListPaginationBox(header, pageCommand, lines);
+    }
 
-        public ListPaginationBox(String header, String pageCommand, List<String> lines) {
-            this(header, pageCommand, (Collection) lines);
-        }
+    private static class ListPaginationBox extends PaginationBox {
+        private final List<Component> lines;
 
-        public ListPaginationBox(String header, String pageCommand, Collection lines) {
+        ListPaginationBox(String header, String pageCommand, Collection<Component> lines) {
             super(header, pageCommand);
             this.lines = lines;
         }
 
         @Override
         public Component getComponent(int number) {
-            Object obj;
-            if (lines instanceof List) {
-                obj = ((List) lines).get(number);
-            } else {
-                if (iterator == null || iterIndex > number) {
-                    iterator = lines.iterator();
-                    iterIndex = 0;
-                }
-                do {
-                    obj = iterator.next();
-                    iterIndex++;
-                } while (iterIndex < number);
-            }
-            if (obj instanceof Supplier) {
-                obj = ((Supplier) obj).get();
-            }
-            if (obj instanceof Component) {
-                return (Component) obj;
-            }
-            return TextComponent.of(obj + "");
+            return lines.get(number);
         }
 
         @Override