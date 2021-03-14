786baf09f44d4a4ae6dab54cac925264994ec348
NotMyFault <mc.cache@web.de>
2021-03-14 15:19:07 +0100

    Closes #957

--- worldedit-bukkit/build.gradle.kts
+++ worldedit-bukkit/build.gradle.kts
@@ -121,8 +121,8 @@ dependencies {
         exclude("com.sk89q.worldedit.worldedit-libs", "bukkit")
         exclude("com.sk89q.worldedit.worldedit-libs", "core")
     }
-    compileOnly("net.kyori:adventure-api:4.6.0")
-    testCompileOnly("net.kyori:adventure-api:4.6.0")
+    compileOnly("net.kyori:adventure-api:4.7.0")
+    testCompileOnly("net.kyori:adventure-api:4.7.0")
     api("com.intellectualsites.paster:Paster:1.0.1-SNAPSHOT")
     // Third party
     implementation("org.bstats:bstats-bukkit:2.2.1")
