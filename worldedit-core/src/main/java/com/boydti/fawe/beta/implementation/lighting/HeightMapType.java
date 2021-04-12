a8001ad71651b8777fb2ccaad5fc175046a0b7e0
Hannes Greule <hannesgreule@outlook.de>
2021-04-12 12:59:21 +0200

    Recognize fluid for waterlogged blocks

--- worldedit-core/src/main/java/com/boydti/fawe/beta/implementation/lighting/HeightMapType.java
+++ worldedit-core/src/main/java/com/boydti/fawe/beta/implementation/lighting/HeightMapType.java
@@ -1,5 +1,7 @@
 package com.boydti.fawe.beta.implementation.lighting;
 
+import com.sk89q.worldedit.registry.state.Property;
+import com.sk89q.worldedit.registry.state.PropertyKey;
 import com.sk89q.worldedit.world.block.BlockCategories;
 import com.sk89q.worldedit.world.block.BlockState;
 
@@ -7,13 +9,13 @@ public enum HeightMapType {
     MOTION_BLOCKING {
         @Override
         public boolean blocks(BlockState state) {
-            return state.getMaterial().isSolid() || state.getMaterial().isLiquid();
+            return state.getMaterial().isSolid() || HeightMapType.isFluid(state);
         }
     },
     MOTION_BLOCKING_NO_LEAVES {
         @Override
         public boolean blocks(BlockState state) {
-            return (state.getMaterial().isSolid() || state.getMaterial().isLiquid()) && !HeightMapType.isLeaf(state);
+            return (state.getMaterial().isSolid() || HeightMapType.isFluid(state)) && !HeightMapType.isLeaf(state);
         }
     },
     OCEAN_FLOOR {
@@ -31,6 +33,13 @@ public enum HeightMapType {
 
     private static boolean isLeaf(BlockState state) {
         return BlockCategories.LEAVES.contains(state);
+    }
+
+    private static boolean isFluid(BlockState state) {
+        if (state.getMaterial().isLiquid()) return true;
+        Property<Boolean> waterlogged = state.getBlockType().getProperty(PropertyKey.WATERLOGGED);
+        if (waterlogged == null) return false;
+        return state.getState(waterlogged);
     }
 
     public abstract boolean blocks(BlockState state);
