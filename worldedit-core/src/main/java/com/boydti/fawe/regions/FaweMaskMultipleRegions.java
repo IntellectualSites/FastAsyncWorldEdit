98a671f34835547d943fa596bbc78b451a21aee1
DolphinGamerYT <dolphingamerytof@gmail.com>
2021-05-30 20:06:43 +0200

    Documented FaweMaskMultipleRegions.java

--- worldedit-core/src/main/java/com/boydti/fawe/regions/FaweMaskMultipleRegions.java
+++ worldedit-core/src/main/java/com/boydti/fawe/regions/FaweMaskMultipleRegions.java
@@ -6,18 +6,33 @@ public class FaweMaskMultipleRegions extends FaweMask {
 
     private final Region[] regions;
 
+    /**
+     * Create a mask with multiple regions in it.
+     *
+     * @param regions Regions that will be masked
+     */
     public FaweMaskMultipleRegions(Region[] regions) {
         super(null);
 
         this.regions = regions;
     }
 
+    /**
+     * NOT USE!!! Used by FaweMask to get the mask.
+     *
+     * @return first region on the regions array
+     */
     @Deprecated
     @Override
     public Region getRegion() {
         return regions[0];
     }
 
+    /**
+     * Get the regions that will be masked.
+     *
+     * @return array of regions
+     */
     public Region[] getRegions() {
         return regions;
     }
