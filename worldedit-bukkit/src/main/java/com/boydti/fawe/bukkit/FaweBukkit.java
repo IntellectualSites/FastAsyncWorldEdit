0af60e5680bc0aed1e04e359f4148ccebdf385ae
NotMyFault <mc.cache@web.de>
2021-06-17 10:37:53 +0200

    Added support for GriefDefender (#1123)

--- worldedit-bukkit/src/main/java/com/boydti/fawe/bukkit/FaweBukkit.java
+++ worldedit-bukkit/src/main/java/com/boydti/fawe/bukkit/FaweBukkit.java
@@ -12,6 +12,7 @@ import com.boydti.fawe.bukkit.listener.BrushListener;
 import com.boydti.fawe.bukkit.listener.ChunkListener9;
 import com.boydti.fawe.bukkit.listener.RenderListener;
 import com.boydti.fawe.bukkit.regions.GriefPreventionFeature;
+import com.boydti.fawe.bukkit.regions.GriefDefenderFeature;
 import com.boydti.fawe.bukkit.regions.ResidenceFeature;
 import com.boydti.fawe.bukkit.regions.TownyFeature;
 import com.boydti.fawe.bukkit.regions.Worldguard;
@@ -213,7 +214,16 @@ public class FaweBukkit implements IFawe, Listener {
         if (griefpreventionPlugin != null && griefpreventionPlugin.isEnabled()) {
             try {
                 managers.add(new GriefPreventionFeature(griefpreventionPlugin));
-                LOGGER.debug("Attempting to use plugin 'GriefPrevention'");
+                LOGGER.info("Attempting to use plugin 'GriefPrevention'");
+            } catch (Throwable ignored) {
+            }
+        }
+        final Plugin griefdefenderPlugin =
+                Bukkit.getServer().getPluginManager().getPlugin("GriefDefender");
+        if (griefdefenderPlugin != null && griefdefenderPlugin.isEnabled()) {
+            try {
+                managers.add(new GriefDefenderFeature(griefdefenderPlugin));
+                LOGGER.info("Attempting to use plugin 'GriefDefender'");
             } catch (Throwable ignored) {
             }
         }
