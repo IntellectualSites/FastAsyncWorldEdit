5073532fd7c21a59e71f87cae0319b1ab5600e33
NotMyFault <mc.cache@web.de>
2021-07-01 11:35:25 +0200

    Hurr durr I don't want to update Java

--- buildSrc/src/main/kotlin/PlatformConfig.kt
+++ buildSrc/src/main/kotlin/PlatformConfig.kt
@@ -38,6 +38,7 @@ fun Project.applyPlatformAndCoreConfiguration() {
             val disabledLint = listOf(
                 "processing", "path", "fallthrough", "serial"
             )
+            options.release.set(11)
             //options.compilerArgs.addAll(listOf("-Xlint:all") + disabledLint.map { "-Xlint:-$it" })
             options.isDeprecation = false
             options.encoding = "UTF-8"
