34e18ff730a0ae078bbea8ae940726baadbcc19e
dordsor21 <dordsor21@gmail.com>
2021-09-10 14:57:40 +0100

    Deprecate sponge schematic reader/writer and tell users why

--- worldedit-core/src/main/java/com/sk89q/worldedit/extent/clipboard/io/SpongeSchematicReader.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/extent/clipboard/io/SpongeSchematicReader.java
@@ -68,7 +68,12 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Reads schematic files using the Sponge Schematic Specification.
+ *
+ * @deprecated Slow, resource intensive, but sometimes safer than using the recommended
+ * {@link com.fastasyncworldedit.core.extent.clipboard.io.FastSchematicReader}.
+ * Avoid reading large schematics with this reader.
  */
+@Deprecated
 public class SpongeSchematicReader extends NBTSchematicReader {
 
     private static final Logger LOGGER = LogManagerCompat.getLogger();
