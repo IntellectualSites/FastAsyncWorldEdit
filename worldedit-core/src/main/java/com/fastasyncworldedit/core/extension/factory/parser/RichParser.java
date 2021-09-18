fad1fcd8ee78bec9d9fcafda0b8b08608f21c192
dordsor21 <dordsor21@gmail.com>
2021-09-18 15:07:41 +0100

    Fix RichParser suggestions

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/extension/factory/parser/RichParser.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/extension/factory/parser/RichParser.java
@@ -51,7 +51,7 @@ public abstract class RichParser<E> extends InputParser<E> implements AliasedPar
     @Nonnull
     private Function<String, Stream<? extends String>> extractArguments(String input) {
         return prefix -> {
-            if (input.length() > prefix.length() && prefix.startsWith(input + "[")) {
+            if (input.length() > prefix.length() && input.startsWith(prefix + "[")) {
                 // input already contains argument(s) -> extract them
                 String[] strings = extractArguments(input.substring(prefix.length()), false);
                 // rebuild the argument string without the last argument
