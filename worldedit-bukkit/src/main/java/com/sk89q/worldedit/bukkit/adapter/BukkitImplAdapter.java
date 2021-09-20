d5be6940c72ea3232ffca4b652ef687f7ff97732
dordsor21 <dordsor21@gmail.com>
2021-09-20 14:46:51 +0100

    Add default getAllProperties method

--- worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/adapter/BukkitImplAdapter.java
+++ worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/adapter/BukkitImplAdapter.java
@@ -63,6 +63,7 @@ import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nullable;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.OptionalInt;
 import java.util.Set;
@@ -332,6 +333,10 @@ public interface BukkitImplAdapter<T> extends IBukkitAdapter {
 
     default RelighterFactory getRelighterFactory() {
         return new NMSRelighterFactory(); // TODO implement in adapters instead
+    }
+
+    default Map<String, ? extends Property<?>> getAllProperties() {
+        return Collections.emptyMap();
     }
     //FAWE end
 }
