121bd58029e166dc4e5d14f1b06401b8f05b9b09
NotMyFault <mc.cache@web.de>
2021-11-23 14:14:01 +0100

    ci: Register javadoc jar for lib artifacts

--- buildSrc/src/main/kotlin/LibsConfig.kt
+++ buildSrc/src/main/kotlin/LibsConfig.kt
@@ -96,8 +96,14 @@ fun Project.applyLibrariesConfiguration() {
         archiveClassifier.set("sources")
     }
 
+    // This a dummy jar to comply with the requirements of the OSSRH,
+    // libs are not API and therefore no "proper" javadoc jar is necessary
+    tasks.register<Jar>("javadocJar") {
+        archiveClassifier.set("javadoc")
+    }
+
     tasks.named("assemble").configure {
-        dependsOn("jar", "sourcesJar")
+        dependsOn("jar", "sourcesJar", "javadocJar")
     }
 
     project.apply<LibsConfigPluginHack>()
@@ -148,6 +154,20 @@ fun Project.applyLibrariesConfiguration() {
         outgoing.artifact(tasks.named("sourcesJar"))
     }
 
+    val javadocElements = project.configurations.register("javadocElements") {
+        isVisible = false
+        description = "Javadoc elements for libs"
+        isCanBeResolved = false
+        isCanBeConsumed = true
+        attributes {
+            attribute(Usage.USAGE_ATTRIBUTE, project.objects.named(Usage.JAVA_RUNTIME))
+            attribute(Category.CATEGORY_ATTRIBUTE, project.objects.named(Category.DOCUMENTATION))
+            attribute(Bundling.BUNDLING_ATTRIBUTE, project.objects.named(Bundling.SHADOWED))
+            attribute(DocsType.DOCS_TYPE_ATTRIBUTE, project.objects.named(DocsType.JAVADOC))
+        }
+        outgoing.artifact(tasks.named("javadocJar"))
+    }
+
     libsComponent.addVariantsFromConfiguration(apiElements.get()) {
         mapToMavenScope("compile")
     }
@@ -157,6 +177,10 @@ fun Project.applyLibrariesConfiguration() {
     }
 
     libsComponent.addVariantsFromConfiguration(sourcesElements.get()) {
+        mapToMavenScope("runtime")
+    }
+
+    libsComponent.addVariantsFromConfiguration(javadocElements.get()) {
         mapToMavenScope("runtime")
     }
 
