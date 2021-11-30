d3aaf7acff64b01c6a0ffef69494c36f4e8021a2
NotMyFault <mc.cache@web.de>
2021-11-30 18:04:47 +0100

    Move to Paper 1.18

--- worldedit-bukkit/adapters/adapter-1_18/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_18/build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
 }
 
 dependencies {
-    paperDevBundle("1.18-rc3-R0.1-20211129.221606-5")
+    paperDevBundle("1.18-rc3-R0.1-20211129.221606-5") //TODO 1.18 switch to mainline
     compileOnly(libs.paperlib)
     compileOnly(libs.paper)
 }
