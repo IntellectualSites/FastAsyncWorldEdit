c9f715c3d7d71b5277fa4a20d5c685821c677d85
NotMyFault <mc.cache@web.de>
2021-12-10 17:15:29 +0100

    build: Re-add 1.17 module
    This the a very nasty commit

--- worldedit-bukkit/adapters/adapter-1_17_1/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_17_1/build.gradle.kts
@@ -15,12 +15,25 @@ plugins {
 }
 
 repositories {
+    mavenCentral()
     maven {
         name = "PaperMC"
         url = uri("https://papermc.io/repo/repository/maven-public/")
+    }
+    maven {
+        name = "Athion"
+        url = uri("https://ci.athion.net/plugin/repository/tools/")
         content {
-            includeModule("io.papermc", "paperlib")
+            includeModule("io.papermc", "paper-server")
         }
+    }
+    maven {
+        name = "OSS Sonatype Snapshots"
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+    maven {
+        name = "Mojang"
+        url = uri("https://libraries.minecraft.net/")
     }
 }
 
@@ -28,6 +41,22 @@ artifacts {
     add("default", file("./src/main/resources/worldedit-adapter-1.17.1.jar"))
 }
 
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+configurations.all {
+    attributes.attribute(TargetJvmVersion.TARGET_JVM_VERSION_ATTRIBUTE, 17)
+}
+
+
 dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.17.1-R0.1-SNAPSHOT")
+    compileOnly(project(":worldedit-bukkit"))
+    compileOnly(project(":worldedit-core"))
+    compileOnly("io.papermc:paper-server:1_17_r1_2")
     compileOnly(libs.paperlib)
+    compileOnly("com.mojang:datafixerupper:4.0.26")
+    compileOnly("com.mojang:authlib:2.3.31")
+    compileOnly("com.mojang:brigadier:1.0.18")
 }
