2495114bbde6c3899c987f89ef53b388dff018fc
SirYwell <hannesgreule@outlook.de>
2021-12-24 08:26:40 +0100

    delay closing of clipboards on quit

--- worldedit-core/src/main/java/com/sk89q/worldedit/entity/Player.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/entity/Player.java
@@ -437,12 +437,14 @@ public interface Player extends Entity, Actor {
                     } else {
                         continue;
                     }
-                    doc.close(); // Ensure closed before deletion
-                    doc.getFile().delete();
+                    WorldEdit.getInstance().getExecutorService().submit(() -> {
+                        doc.close(); // Ensure closed before deletion
+                        doc.getFile().delete();
+                    });
                 }
             }
         } else if (Settings.IMP.CLIPBOARD.DELETE_ON_LOGOUT || Settings.IMP.CLIPBOARD.USE_DISK) {
-            session.setClipboard(null);
+            WorldEdit.getInstance().getExecutorService().submit(() -> session.setClipboard(null));
         }
         if (Settings.IMP.HISTORY.DELETE_ON_LOGOUT) {
             session.clearHistory();
