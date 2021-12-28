3ec19120465fa25bd01046fc6d1f277a6ae03ab6
dordsor21 <dordsor21@gmail.com>
2021-12-28 23:22:59 +0000

    Fix tests

---
+++ worldedit-core/src/test/java/com/fastasyncworldedit/util/StubPlatform.java
@@ -0,0 +1,113 @@
+package com.fastasyncworldedit.util;
+
+import com.fastasyncworldedit.core.extent.processor.lighting.RelighterFactory;
+import com.sk89q.worldedit.LocalConfiguration;
+import com.sk89q.worldedit.entity.Player;
+import com.sk89q.worldedit.extension.platform.AbstractPlatform;
+import com.sk89q.worldedit.extension.platform.Capability;
+import com.sk89q.worldedit.extension.platform.Preference;
+import com.sk89q.worldedit.internal.Constants;
+import com.sk89q.worldedit.util.SideEffect;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldedit.world.registry.Registries;
+import org.enginehub.piston.CommandManager;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Set;
+
+public class StubPlatform extends AbstractPlatform {
+
+    @Override
+    public Registries getRegistries() {
+        return null;
+    }
+
+    @Override
+    public int getDataVersion() {
+        return Constants.DATA_VERSION_MC_1_18;
+    }
+
+    @Override
+    public boolean isValidMobType(final String type) {
+        return false;
+    }
+
+    @Override
+    public void reload() {
+
+    }
+
+    @Nullable
+    @Override
+    public Player matchPlayer(final Player player) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public World matchWorld(final World world) {
+        return null;
+    }
+
+    @Override
+    public void registerCommands(final CommandManager commandManager) {
+
+    }
+
+    @Override
+    public void setGameHooksEnabled(final boolean enabled) {
+
+    }
+
+    @Override
+    public LocalConfiguration getConfiguration() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return "TEST";
+    }
+
+    @Override
+    public String getPlatformName() {
+        return "TEST";
+    }
+
+    @Override
+    public String getPlatformVersion() {
+        return "TEST";
+    }
+
+    @Override
+    public Map<Capability, Preference> getCapabilities() {
+        Map<Capability, Preference> capabilities = new EnumMap<>(Capability.class);
+        capabilities.put(Capability.WORLD_EDITING, Preference.PREFER_OTHERS);
+        return capabilities;
+    }
+
+    @Override
+    public Set<SideEffect> getSupportedSideEffects() {
+        return null;
+    }
+
+    @Override
+    public RelighterFactory getRelighterFactory() {
+        return null;
+    }
+
+    // Use most "extreme" value
+    @Override
+    public int getVersionMinY() {
+        return -64;
+    }
+
+    // Use most "extreme" value
+    @Override
+    public int getVersionMaxY() {
+        return 319;
+    }
+
+}
