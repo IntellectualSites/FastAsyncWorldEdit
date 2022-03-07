b43f3a2d8fd78ebace552cb77064d91fdbce9f9f
Pierre Maurice Schwang <mail@pschwang.eu>
2022-03-07 17:52:00 +0100

    fix: adapt correct biome type

--- worldedit-bukkit/adapters/adapter-1_18_2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_18_R2/PaperweightGetBlocks.java
+++ worldedit-bukkit/adapters/adapter-1_18_2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_18_R2/PaperweightGetBlocks.java
@@ -197,7 +197,7 @@ public class PaperweightGetBlocks extends CharGetBlocks implements BukkitGetBloc
     public BiomeType getBiomeType(int x, int y, int z) {
         LevelChunkSection section = getSections(false)[(y >> 4) - getMinSectionPosition()];
         Holder<Biome> biomes = section.getNoiseBiome(x >> 2, (y & 15) >> 2, z >> 2);
-        return (BiomeType) PaperweightPlatformAdapter.adapt(biomes, serverLevel);
+        return BiomeTypes.get(PaperweightPlatformAdapter.adapt(biomes, serverLevel).unwrapKey().get().location().toString());
     }
 
     @Override
