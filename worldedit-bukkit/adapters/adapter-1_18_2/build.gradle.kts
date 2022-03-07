fb66ba6adf5fbc6572258963dbff453330ff64e8
NotMyFault <mc.cache@web.de>
2022-03-07 21:20:25 +0100

    build: Update paperweight
    Closes #1649

--- worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
@@ -13,6 +13,6 @@ repositories {
 
 dependencies {
     // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperDevBundle("1.18.2-R0.1-20220306.153715-16")
+    paperDevBundle("1.18.2-R0.1-20220306.212825-18")
     compileOnly(libs.paperlib)
 }
