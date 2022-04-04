d70c3a8e517a6c496285c7cdf706689ada996291
Pierre Maurice Schwang <mail@pschwang.eu>
2022-04-04 19:59:26 +0200

    fix: ignore uuid v2 for BukkitPermissionAttachmentManager
    Fixes a memory leak that attaches and caches PermissionAttachments to NPCs. Citiziens uses version 2 UUIDs, which are not used for regular player uuids. Those v2 uuids are now excluded.

--- worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/BukkitPermissionAttachmentManager.java
+++ worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/BukkitPermissionAttachmentManager.java
@@ -2,6 +2,8 @@ package com.fastasyncworldedit.bukkit;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permissible;
+import org.bukkit.permissions.PermissibleBase;
 import org.bukkit.permissions.PermissionAttachment;
 
 import javax.annotation.Nullable;
@@ -12,20 +14,29 @@ public class BukkitPermissionAttachmentManager {
 
     private final WorldEditPlugin plugin;
     private final Map<Player, PermissionAttachment> attachments = new ConcurrentHashMap<>();
+    private final PermissionAttachment noopAttachment;
 
     public BukkitPermissionAttachmentManager(WorldEditPlugin plugin) {
         this.plugin = plugin;
+        this.noopAttachment = new PermissionAttachment(plugin, new PermissibleBase(null));
     }
 
     public PermissionAttachment getOrAddAttachment(@Nullable final Player p) {
         if (p == null) {
             return null;
         }
+        if (p.getUniqueId().version() == 2) {
+            return noopAttachment;
+        }
         return attachments.computeIfAbsent(p, k -> k.addAttachment(plugin));
     }
 
     public void removeAttachment(@Nullable final Player p) {
         if (p == null) {
+            return;
+        }
+        if (p.getUniqueId().version() == 2) {
+            p.removeAttachment(noopAttachment);
             return;
         }
         PermissionAttachment attach = attachments.remove(p);
