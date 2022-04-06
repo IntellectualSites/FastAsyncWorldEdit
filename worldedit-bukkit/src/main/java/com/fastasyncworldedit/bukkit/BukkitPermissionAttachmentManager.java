2483eacff5cd72e53fa0abe6aa67a9a40197c5c9
Pierre Maurice Schwang <mail@pschwang.eu>
2022-04-06 16:22:53 +0200

    Fix memory leak by Citizen NPCs (#1693)
    * fix: ignore uuid v2 for BukkitPermissionAttachmentManager
    
    Fixes a memory leak that attaches and caches PermissionAttachments to NPCs. Citiziens uses version 2 UUIDs, which are not used for regular player uuids. Those v2 uuids are now excluded.
    
    * fix: create Permission when required
    
    * fix: use citizens documented way to check for NPCs

--- worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/BukkitPermissionAttachmentManager.java
+++ worldedit-bukkit/src/main/java/com/fastasyncworldedit/bukkit/BukkitPermissionAttachmentManager.java
@@ -2,6 +2,7 @@ package com.fastasyncworldedit.bukkit;
 
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissibleBase;
 import org.bukkit.permissions.PermissionAttachment;
 
 import javax.annotation.Nullable;
@@ -12,6 +13,7 @@ public class BukkitPermissionAttachmentManager {
 
     private final WorldEditPlugin plugin;
     private final Map<Player, PermissionAttachment> attachments = new ConcurrentHashMap<>();
+    private PermissionAttachment noopAttachment;
 
     public BukkitPermissionAttachmentManager(WorldEditPlugin plugin) {
         this.plugin = plugin;
@@ -21,11 +23,21 @@ public class BukkitPermissionAttachmentManager {
         if (p == null) {
             return null;
         }
+        if (p.hasMetadata("NPC")) {
+            if (this.noopAttachment == null) {
+                this.noopAttachment = new PermissionAttachment(plugin, new PermissibleBase(null));
+            }
+            return noopAttachment;
+        }
         return attachments.computeIfAbsent(p, k -> k.addAttachment(plugin));
     }
 
     public void removeAttachment(@Nullable final Player p) {
         if (p == null) {
+            return;
+        }
+        if (p.hasMetadata("NPC") && noopAttachment != null) {
+            p.removeAttachment(noopAttachment);
             return;
         }
         PermissionAttachment attach = attachments.remove(p);
