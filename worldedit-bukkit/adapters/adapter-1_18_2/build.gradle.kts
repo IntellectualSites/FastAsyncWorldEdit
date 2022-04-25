c6190de297bd0481184c1a0c7a07cfe003cffcd5
NotMyFault <mc.cache@web.de>
2022-04-25 17:05:24 +0200

    build: Update paperweight

--- worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
@@ -13,6 +13,6 @@ repositories {
 
 dependencies {
     // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperDevBundle("1.18.2-R0.1-20220309.091840-21")
+    paperDevBundle("1.18.2-R0.1-20220424.184034-95")
     compileOnly(libs.paperlib)
 }
