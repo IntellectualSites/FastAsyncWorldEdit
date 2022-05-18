02c08356c8b7547a13c092857cb7553d965d7492
dordsor21 <dordsor21@gmail.com>
2022-05-18 13:54:48 +0100

    Add since tags

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/concurrent/ReentrantWrappedStampedLock.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/concurrent/ReentrantWrappedStampedLock.java
@@ -9,6 +9,8 @@ import java.util.concurrent.locks.StampedLock;
 
 /**
  * Allows for reentrant behaviour of a wrapped {@link StampedLock}. Will not count the number of times it is re-entered.
+ *
+ * @since TODO
  */
 public class ReentrantWrappedStampedLock implements Lock {
 
@@ -82,6 +84,7 @@ public class ReentrantWrappedStampedLock implements Lock {
      *
      * @return lock stam[ or 0 if not locked.
      * @throws IllegalCallerException if the {@link StampedLock} is write-locked and the calling thread is not the lock owner
+     * @since TODO
      */
     public long getStampChecked() {
         if (stamp != 0 && owner != Thread.currentThread()) {
@@ -95,6 +98,7 @@ public class ReentrantWrappedStampedLock implements Lock {
      *
      * @param stamp Stamp to unlock with
      * @throws IllegalMonitorStateException if the given stamp does not match the lock's stamp
+     * @since TODO
      */
     public void unlock(final long stamp) {
         parent.unlockWrite(stamp);
@@ -106,6 +110,7 @@ public class ReentrantWrappedStampedLock implements Lock {
      * Returns true if the lock is currently held.
      *
      * @return true if the lock is currently held.
+     * @since TODO
      */
     public boolean isLocked() {
         return owner == null && this.stamp == 0 && parent.isWriteLocked(); // Be verbose
