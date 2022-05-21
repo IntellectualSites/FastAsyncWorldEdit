d1f3ac8f767dabe2d83e0dbae20fb1dd20cd5e14
BlockyTheDev <86119630+BlockyTheDev@users.noreply.github.com>
2022-05-21 12:08:10 +0200

    chore: Update PaperMC repository URL (#1756)
    * Migrate PaperMC Repository Endpoints
    
    * Migrate PaperMC JavaDocs endpoints

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
     }
     maven {
         name = "PaperMC"
-        url = uri("https://papermc.io/repo/repository/maven-public/")
+        url = uri("https://repo.papermc.io/repository/maven-public/")
         content {
             includeGroupByRegex("io\\.papermc\\..*")
         }
