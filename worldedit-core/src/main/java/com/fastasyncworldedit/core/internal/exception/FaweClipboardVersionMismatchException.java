0b33fa87570482ff41f3f579608534feac5afb74
Jordan <dordsor21@gmail.com>
2022-06-09 11:48:35 +0100

    Allow NBT stored in DiskOptimizedClipboards to be written to disk as a compressed byte array at the end of the file (#1745)
    * Allow NBT stored in DiskOptimizedClipboards to be written to disk as a compressed byte array at the end of the file
    
    * Add some deprecations/javadocs and provide the expected clipboard version on error
    
    * Javadoc since tags and add location of clipboard folder to error
    
    * Refactor load-from-file method into DOC class
    
    * Refactor nbt loading code into separate method in DOC

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/internal/exception/FaweClipboardVersionMismatchException.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/internal/exception/FaweClipboardVersionMismatchException.java
@@ -1,11 +1,62 @@
 package com.fastasyncworldedit.core.internal.exception;
 
+import com.fastasyncworldedit.core.Fawe;
 import com.fastasyncworldedit.core.configuration.Caption;
+import com.fastasyncworldedit.core.configuration.Settings;
+import com.fastasyncworldedit.core.extent.clipboard.DiskOptimizedClipboard;
+
+import java.io.File;
 
 public class FaweClipboardVersionMismatchException extends FaweException {
 
+    private final int expected;
+    private final int version;
+
+    /**
+     * @deprecated Use {@link FaweClipboardVersionMismatchException#FaweClipboardVersionMismatchException(int, int)}
+     */
+    @Deprecated(forRemoval = true, since = "TODO")
     public FaweClipboardVersionMismatchException() {
-        super(Caption.of("fawe.error.clipboard.on.disk.version.mismatch"), Type.CLIPBOARD);
+        this(DiskOptimizedClipboard.VERSION, -1);
+    }
+
+    /**
+     * New exception specifying a version mismatch between that supported and that loaded.
+     *
+     * @param version  version of clipboard attempting to be loaded
+     * @param expected expected version of clipboard
+     * @since TODO
+     */
+    public FaweClipboardVersionMismatchException(int expected, int version) {
+        super(
+                Caption.of(
+                        "fawe.error.clipboard.on.disk.version.mismatch",
+                        expected,
+                        version,
+                        Fawe.platform().getDirectory().getName() + File.separator + Settings.settings().PATHS.CLIPBOARD
+                ),
+                Type.CLIPBOARD
+        );
+        this.expected = expected;
+        this.version = version;
+    }
+
+    /**
+     * Get the version specified in the clipboard attempting to be loaded.
+     *
+     * @since TODO
+     */
+    public int getClipboardVersion() {
+        return version;
+    }
+
+    /**
+     * Get the version that was expected of the clipboard
+     *
+     * @since TODO
+     */
+    public int getExpectedVersion() {
+        return expected;
     }
 
 }
