c3faf8b331e2c56bc2b8ff65550bd62c0b61ec69
Alexander Brandes <mc.cache@web.de>
2022-08-06 17:38:06 +0200

    build: Update paperweight

--- worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_18_2/build.gradle.kts
@@ -13,6 +13,6 @@ repositories {
 
 dependencies {
     // https://papermc.io/repo/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperDevBundle("1.18.2-R0.1-20220614.230913-165")
+    paperDevBundle("1.18.2-R0.1-20220703.182221-166")
     compileOnly("io.papermc:paperlib")
 }
