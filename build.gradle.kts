18bf911fdc2bce8724f1645ede4d3f7e9a7d7f82
Alexander Brandes <mc.cache@web.de>
2022-10-04 14:05:36 +0200

    Release 2.4.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ ext {
     }
 }
 
-version = String.format("%s-%s", rootVersion, buildNumber)
+version = String.format("%s", rootVersion)
 
 if (!project.hasProperty("gitCommitHash")) {
     apply(plugin = "org.ajoberstar.grgit")
