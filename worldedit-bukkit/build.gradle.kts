d3ef8a028b980348650b4d832801b4a893dd3bc1
Alexander Brandes <mc.cache@web.de>
2022-10-04 10:04:04 +0200

    chore: Link to GH releases

--- worldedit-bukkit/build.gradle.kts
+++ worldedit-bukkit/build.gradle.kts
@@ -199,5 +199,7 @@ tasks {
         uploadFile.set(file("build/libs/${rootProject.name}-Bukkit-${project.version}.jar"))
         gameVersions.addAll(listOf("1.19.2", "1.19.1", "1.19", "1.18.2", "1.17.1", "1.16.5"))
         loaders.addAll(listOf("paper", "purpur", "spigot"))
+        changelog.set("The changelog is available on GitHub: https://github.com/IntellectualSites/" +
+                "FastAsyncWorldEdit/releases/tag/${project.version}")
     }
 }
