2a6812c27a5c37ed40ca64d22b4141380fcc4d02
Alexander Brandes <mc.cache@web.de>
2022-12-09 12:26:25 +0100

    Update dev bundle versions

--- worldedit-bukkit/adapters/adapter-1_19/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_19/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 }
 
 dependencies {
-    paperDevBundle("1.19.2-R0.1-20221029.172539-134")
+    paperDevBundle("1.19.2-R0.1-20221206.184705-189")
     compileOnly("io.papermc:paperlib")
 }
