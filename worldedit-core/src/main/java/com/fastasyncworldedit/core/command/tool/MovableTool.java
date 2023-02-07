1e5b2fe0de80cd10e1d7ea185273d0011a4e9f10
Alexander Brandes <mc.cache@web.de>
2023-02-07 23:25:17 +0100

    Back to snapshot for development

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/command/tool/MovableTool.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/command/tool/MovableTool.java
@@ -5,13 +5,13 @@ import com.sk89q.worldedit.entity.Player;
 /**
  * @deprecated This interface has no functionality anymore
  */
-@Deprecated(forRemoval = true, since = "TODO")
+@Deprecated(forRemoval = true, since = "2.5.2")
 public interface MovableTool {
 
     /**
      * @deprecated This interface has no functionality anymore
      */
-    @Deprecated(forRemoval = true, since = "TODO")
+    @Deprecated(forRemoval = true, since = "2.5.2")
     boolean move(Player player);
 
 }
