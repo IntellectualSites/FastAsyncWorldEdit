b9dbdfb5317459182e96d7e82c2b871dade8a944
Alexander Brandes <mc.cache@web.de>
2023-03-06 15:31:29 +0100

    Misc. dependency update to align with libs.versions.toml

--- buildSrc/src/main/kotlin/CommonConfig.kt
+++ buildSrc/src/main/kotlin/CommonConfig.kt
@@ -45,15 +45,15 @@ fun Project.applyCommonConfiguration() {
                     continue
                 }
                 add(conf.name, "com.google.guava:guava") {
-                    version { require("31.0.1-jre") }
+                    version { require("31.1-jre") }
                     because("Mojang provides Guava")
                 }
                 add(conf.name, "com.google.code.gson:gson") {
-                    version { require("2.8.8") }
+                    version { require("2.10") }
                     because("Mojang provides Gson")
                 }
                 add(conf.name, "it.unimi.dsi:fastutil") {
-                    version { require("8.5.6") }
+                    version { require("8.5.9") }
                     because("Mojang provides FastUtil")
                 }
             }
