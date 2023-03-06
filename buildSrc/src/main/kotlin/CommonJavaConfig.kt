992eaf2b4c60280f09dcc291ffe08f0bfe5a1598
Alexander Brandes <mc.cache@web.de>
2023-03-06 15:44:21 +0100

    Misc. dependency update to align with libs.versions.toml (#2134)

--- buildSrc/src/main/kotlin/CommonJavaConfig.kt
+++ buildSrc/src/main/kotlin/CommonJavaConfig.kt
@@ -40,11 +40,11 @@ fun Project.applyCommonJavaConfiguration(sourcesJar: Boolean, banSlf4j: Boolean 
 
     dependencies {
         "compileOnly"("com.google.code.findbugs:jsr305:3.0.2")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.8.1")
-        "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.8.1")
-        "testImplementation"("org.mockito:mockito-core:3.12.4")
-        "testImplementation"("org.mockito:mockito-junit-jupiter:3.12.4")
-        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+        "testImplementation"("org.junit.jupiter:junit-jupiter-api:5.9.2")
+        "testImplementation"("org.junit.jupiter:junit-jupiter-params:5.9.2")
+        "testImplementation"("org.mockito:mockito-core:5.1.1")
+        "testImplementation"("org.mockito:mockito-junit-jupiter:5.1.1")
+        "testRuntimeOnly"("org.junit.jupiter:junit-jupiter-engine:5.9.2")
         "implementation"(platform("com.intellectualsites.bom:bom-1.18.x:1.9"))
     }
 
@@ -64,7 +64,7 @@ fun Project.applyCommonJavaConfiguration(sourcesJar: Boolean, banSlf4j: Boolean 
                     "https://www.antlr.org/api/Java/",
                     "https://docs.enginehub.org/javadoc/org.enginehub.piston/core/0.5.7/",
                     "https://docs.enginehub.org/javadoc/org.enginehub.piston/default-impl/0.5.7/",
-                    "https://jd.papermc.io/paper/1.18/",
+                    "https://jd.papermc.io/paper/1.19/",
                     "https://intellectualsites.github.io/fastasyncworldedit-javadocs/worldedit-core/"
             )
             docTitle = "${rootProject.name}-${project.description}" +  " " + "${rootProject.version}"
