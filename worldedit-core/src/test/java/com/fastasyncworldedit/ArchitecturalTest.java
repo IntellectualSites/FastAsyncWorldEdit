95b77c15727bc696f7e7b810f5bcb6babfb456d3
SirYwell <hannesgreule@outlook.de>
2023-03-08 18:10:38 +0100

    actually delegate in AbstractDelegateExtent

---
+++ worldedit-core/src/test/java/com/fastasyncworldedit/ArchitecturalTest.java
@@ -0,0 +1,28 @@
+package com.fastasyncworldedit;
+
+import com.sk89q.worldedit.extent.AbstractDelegateExtent;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ArchitecturalTest {
+
+    public static Stream<Arguments> abstractDelegateExtentMethods() {
+        return Arrays.stream(AbstractDelegateExtent.class.getMethods())
+                .filter(m -> m.getDeclaringClass() != Object.class) // ignore methods inherited from java.lang.Object
+                .map(Arguments::of);
+    }
+
+    @ParameterizedTest
+    @MethodSource("abstractDelegateExtentMethods")
+    void testAbstractDelegateExtentOverridesAllMethods(Method method) {
+        assertEquals(AbstractDelegateExtent.class, method.getDeclaringClass());
+    }
+
+}
