f81fcae5e1c2cacdbf94e6320fca23ffc60dd3ce
Maddy Miller <mnmiller1@me.com>
2023-03-10 21:39:24 +1000

    Add a feature generator and allow undoing of feature placement (https://github.com/EngineHub/WorldEdit/pull/2239)
    * Add a feature generator and allow undoing of feature placement [WIP]
    
    Apply changes to Forge as well
    
    Use proper translatable components
    
    * Add a brush version of the feature command
    
    Use Java proxy classes
    
    * Add for Bukkit (only 1.19.3 for now)
    
    Clean up the proxies to use a switch
    
    Checkstyle is grumpy
    
    Add the obfuscated versions
    
    Remove debug text
    
    Fix missed "destroyBlock" deobfuscated proxy function
    
    * checkstyle

--- worldedit-core/src/main/java/com/sk89q/worldedit/command/BrushCommands.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/command/BrushCommands.java
@@ -66,6 +66,7 @@ import com.sk89q.worldedit.LocalSession;
 import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.command.argument.Arguments;
+import com.sk89q.worldedit.command.factory.FeatureGeneratorFactory;
 import com.sk89q.worldedit.command.factory.ReplaceFactory;
 import com.sk89q.worldedit.command.factory.TreeGeneratorFactory;
 import com.sk89q.worldedit.command.tool.BrushTool;
@@ -116,6 +117,7 @@ import com.sk89q.worldedit.world.block.BlockStateHolder;
 import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.block.BlockTypes;
 import org.anarres.parallelgzip.ParallelGZIPOutputStream;
+import com.sk89q.worldedit.world.generation.ConfiguredFeatureType;
 import org.enginehub.piston.annotation.Command;
 import org.enginehub.piston.annotation.CommandContainer;
 import org.enginehub.piston.annotation.param.Arg;
@@ -1554,6 +1556,24 @@ public class BrushCommands {
         flags.or(CreatureButcher.Flags.WATER, killWater, "worldedit.butcher.water");
 
         set(context, new ButcherBrush(flags), "worldedit.brush.butcher").setSize(radius);
+    }
+
+    @Command(
+            name = "feature",
+            desc = "Feature brush, paints Minecraft generation features"
+    )
+    @CommandPermissions("worldedit.brush.feature")
+    public void feature(Player player, LocalSession localSession,
+                        @Arg(desc = "The shape of the region")
+                        RegionFactory shape,
+                        @Arg(desc = "The size of the brush", def = "5")
+                        double radius,
+                        @Arg(desc = "The density of the brush", def = "5")
+                        double density,
+                        @Arg(desc = "The type of feature to use")
+                        ConfiguredFeatureType type) throws WorldEditException {
+        setOperationBasedBrush(player, localSession, radius,
+                new Paint(new FeatureGeneratorFactory(type), density / 100), shape, "worldedit.brush.feature");
     }
 
     //FAWE start
