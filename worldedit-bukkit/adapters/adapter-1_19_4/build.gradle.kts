80adc5817af527a52f6e72e0eed8cda88357df51
Alexander Brandes <mc.cache@web.de>
2023-03-17 22:09:38 +0100

    Update refractions for regen support

--- worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    paperDevBundle("1.19.4-R0.1-20230315.180941-2")
+    paperDevBundle("1.19.4-R0.1-20230317.182750-6")
     compileOnly("io.papermc:paperlib")
 }
