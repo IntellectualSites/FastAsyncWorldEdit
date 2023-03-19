eb1c9dc4db28fc16bef02cffc441011869410fa4
Alexander Brandes <mc.cache@web.de>
2023-03-19 10:34:49 +0100

    Apply stash

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ ext {
     }
 }
 
-version = String.format("%s", rootVersion)
+version = String.format("%s-%s", rootVersion, buildNumber)
 
 if (!project.hasProperty("gitCommitHash")) {
     apply(plugin = "org.ajoberstar.grgit")
