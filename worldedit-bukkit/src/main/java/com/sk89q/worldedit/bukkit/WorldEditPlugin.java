677651f909f529f023536227757177d52568ae5b
Phillip Glanz <p.glanz@madfix.me>
2023-04-02 23:25:22 +0200

    Fix UnsupportedOperationException on disable

--- worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
+++ worldedit-bukkit/src/main/java/com/sk89q/worldedit/bukkit/WorldEditPlugin.java
@@ -21,6 +21,7 @@ package com.sk89q.worldedit.bukkit;
 
 import com.fastasyncworldedit.bukkit.BukkitPermissionAttachmentManager;
 import com.fastasyncworldedit.bukkit.FaweBukkit;
+import com.fastasyncworldedit.core.util.FoliaSupport;
 import com.fastasyncworldedit.core.util.UpdateNotification;
 import com.fastasyncworldedit.core.Fawe;
 import com.fastasyncworldedit.core.util.WEManager;
@@ -400,7 +401,9 @@ public class WorldEditPlugin extends JavaPlugin {
         if (config != null) {
             config.unload();
         }
-        this.getServer().getScheduler().cancelTasks(this);
+        if (!FoliaSupport.isFolia()) {
+            this.getServer().getScheduler().cancelTasks(this);
+        }
     }
 
     /**
