47f3ac6ec43c0517a7c95ff5e8f9a71f7899e0ce
Phillip Glanz <p.glanz@madfix.me>
2023-04-02 21:36:16 +0200

    Fix shading of folia adapters

--- worldedit-bukkit/build.gradle.kts
+++ worldedit-bukkit/build.gradle.kts
@@ -60,7 +60,15 @@ val adapters = configurations.create("adapters") {
         )
     }
 }
-
+val foliaAdapters = configurations.create("foliaAdapters") {
+    description = "Adapters to include in the JAR"
+    isCanBeConsumed = false
+    isCanBeResolved = true
+    shouldResolveConsistentlyWith(configurations["runtimeClasspath"])
+    attributes {
+        attribute(Obfuscation.OBFUSCATION_ATTRIBUTE, objects.named(Obfuscation.OBFUSCATED))
+    }
+}
 dependencies {
     // Modules
     api(projects.worldeditCore)
@@ -70,7 +78,7 @@ dependencies {
         "adapters"(project(it.path))
     }
     project.project(":worldedit-bukkit:folia-adapters").subprojects.forEach {
-        "adapters"(project(it.path))
+        "foliaAdapters"(project(it.path))
     }
 
     // Minecraft expectations
@@ -148,6 +156,14 @@ tasks.named<ShadowJar>("shadowJar") {
     dependsOn(project.project(":worldedit-bukkit:folia-adapters").subprojects.map { it.tasks.named("assemble") })
     from(Callable {
         adapters.resolve()
+                .map { f ->
+                    zipTree(f).matching {
+                        exclude("META-INF/")
+                    }
+                }
+    })
+    from(Callable {
+        foliaAdapters.resolve()
                 .map { f ->
                     zipTree(f).matching {
                         exclude("META-INF/")
