a6e59505b412bf2be78e01ab56a43eede7ad57e1
Phillip Glanz <p.glanz@madfix.me>
2023-04-02 17:32:30 +0200

    Set paperweight to local dev build

--- worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
@@ -5,10 +5,12 @@ plugins {
 applyPaperweightAdapterConfiguration()
 
 repositories {
+    mavenLocal()
     gradlePluginPortal()
 }
 
 dependencies {
-    paperDevBundle("1.19.4-R0.1-20230317.182750-6")
+    paperweightDevBundle("dev.folia", "1.19.4-R0.1-SNAPSHOT")
+    //paperDevBundle("1.19.4-R0.1-20230317.182750-6")
     compileOnly("io.papermc:paperlib")
 }
