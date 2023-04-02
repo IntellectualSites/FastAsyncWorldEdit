84e7dd4c5c2fed54c51f845d019b394d0b8e11a6
Phillip Glanz <p.glanz@madfix.me>
2023-04-02 20:43:09 +0200

    Build back to paper adapter

--- worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
@@ -9,7 +9,6 @@ repositories {
 }
 
 dependencies {
-    paperweightDevBundle("dev.folia", "1.19.4-R0.1-SNAPSHOT")
-    //paperDevBundle("1.19.4-R0.1-20230317.182750-6")
+    paperDevBundle("1.19.4-R0.1-20230317.182750-6")
     compileOnly("io.papermc:paperlib")
 }
