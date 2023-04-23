abe120291f99a505ec68ec2d8dd902f2e53bda16
Alexander Brandes <mc.cache@web.de>
2023-04-23 22:47:30 +0200

    Release 2.6.1

--- worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_19_4/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    paperDevBundle("1.19.4-R0.1-20230412.010331-64")
+    paperDevBundle("1.19.4-R0.1-20230423.020222-72")
     compileOnly("io.papermc:paperlib")
 }
