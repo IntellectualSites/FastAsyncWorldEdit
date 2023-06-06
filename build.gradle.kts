bdb170a5ca5b6209f3ce48cea6961730c6c20686
Alexander Brandes <mc.cache@web.de>
2023-06-06 19:18:01 +0200

    Explicit call repository task

--- build.gradle.kts
+++ build.gradle.kts
@@ -105,7 +105,7 @@ tasks {
 }
 
 nexusPublishing {
-    repositories {
+    this.repositories {
         sonatype {
             nexusUrl.set(URI.create("https://s01.oss.sonatype.org/service/local/"))
             snapshotRepositoryUrl.set(URI.create("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
