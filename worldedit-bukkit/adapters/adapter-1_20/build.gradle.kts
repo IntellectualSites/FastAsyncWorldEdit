e754f35abe08d6724c1fb35f1a184a6e44e1627b
TheMeinerLP <p.glanz@madfix.me>
2023-06-18 17:38:28 +0200

    Add comment out folia dependency

--- worldedit-bukkit/adapters/adapter-1_20/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    // // the<PaperweightUserDependenciesExtension>().foliaDevBundle("1.20.1-R0.1-SNAPSHOT")
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.1-R0.1-20230916.212543-167")
     compileOnly(libs.paperlib)
 }
