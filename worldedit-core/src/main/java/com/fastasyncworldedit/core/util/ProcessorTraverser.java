d6f751666172e58672d7ec8f29a7b623a4289118
dordsor21 <dordsor21@gmail.com>
2023-07-14 09:56:46 +0100

    Clean up ProcessorTraverser

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/util/ProcessorTraverser.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/util/ProcessorTraverser.java
@@ -3,6 +3,8 @@ package com.fastasyncworldedit.core.util;
 import com.fastasyncworldedit.core.extent.processor.BatchProcessorHolder;
 import com.fastasyncworldedit.core.extent.processor.MultiBatchProcessor;
 import com.fastasyncworldedit.core.queue.IBatchProcessor;
+import com.sk89q.worldedit.internal.util.LogManagerCompat;
+import org.apache.logging.log4j.Logger;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -11,36 +13,30 @@ import java.util.Queue;
 
 public class ProcessorTraverser<T extends IBatchProcessor> {
 
+    private static final Logger LOGGER = LogManagerCompat.getLogger();
+
     private final T root;
 
     public ProcessorTraverser(@Nonnull T root) {
         this.root = root;
     }
 
-    @SuppressWarnings("unchecked")
-    public @Nullable <U extends IBatchProcessor> U find(Class<U> clazz) {
+    public <U extends IBatchProcessor> @Nullable U find(Class<U> clazz) {
         try {
-            Queue<IBatchProcessor> processors = new ArrayDeque<>(5);
-            if (this.root instanceof MultiBatchProcessor multiBatchProcessor) {
-                processors.addAll(multiBatchProcessor.getBatchProcessors());
-            } else if (this.root instanceof BatchProcessorHolder holder) {
-                processors.add(holder.getProcessor());
-            } else {
-                return clazz.isAssignableFrom(this.root.getClass()) ? clazz.cast(this.root) : null;
-            }
-            IBatchProcessor processor;
-            while ((processor = processors.poll()) != null) {
+            Queue<IBatchProcessor> processors = new ArrayDeque<>();
+            IBatchProcessor processor = root;
+            do {
                 if (clazz.isAssignableFrom(processor.getClass())) {
-                    return (U) processor;
+                    return clazz.cast(processor);
                 } else if (processor instanceof MultiBatchProcessor multiProcessor) {
                     processors.addAll(multiProcessor.getBatchProcessors());
                 } else if (processor instanceof BatchProcessorHolder holder) {
                     processors.add(holder.getProcessor());
                 }
-            }
+            } while ((processor = processors.poll()) != null);
             return null;
         } catch (Throwable e) {
-            e.printStackTrace();
+            LOGGER.error("Error traversing processors", e);
             return null;
         }
     }
