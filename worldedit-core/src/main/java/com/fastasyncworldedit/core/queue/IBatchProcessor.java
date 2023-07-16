b4fe594781bfb856240507b9b01af4ecc0ce7335
Paul19988 <paul@paulcodes.co.uk>
2023-07-17 00:17:02 +0100

    fix regions not trimming Y when restricted correctly

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/queue/IBatchProcessor.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/queue/IBatchProcessor.java
@@ -85,7 +85,7 @@ public interface IBatchProcessor {
             }
             for (int layer = maxLayer; layer < set.getMaxSectionPosition(); layer++) {
                 if (set.hasSection(layer)) {
-                    if (layer == minLayer) {
+                    if (layer == maxLayer) {
                         char[] arr = set.loadIfPresent(layer);
                         if (arr != null) {
                             int index = ((maxY + 1) & 15) << 8;
@@ -126,7 +126,7 @@ public interface IBatchProcessor {
                 hasBlocks |= set.hasSection(layer);
                 continue;
             }
-            if (layer == minLayer) {
+            if (layer == maxLayer) {
                 char[] arr = set.loadIfPresent(layer);
                 if (arr != null) {
                     int index = (minY & 15) << 8;
