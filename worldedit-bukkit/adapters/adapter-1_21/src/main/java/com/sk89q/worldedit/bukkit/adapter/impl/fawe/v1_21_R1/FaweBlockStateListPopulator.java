25545fdccc24a4aac8cb1c33bfc6ffd92f0c5d7c
dordsor21 <dordsor21@gmail.com>
2023-10-24 17:32:55 +0100

    FAWE-inate

---
+++ worldedit-bukkit/adapters/adapter-1_21/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_21_R1/FaweBlockStateListPopulator.java
@@ -0,0 +1,100 @@
+package com.sk89q.worldedit.bukkit.adapter.impl.fawe.v1_21_R1;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.BiomeManager;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.border.WorldBorder;
+import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkSource;
+import net.minecraft.world.level.chunk.status.ChunkStatus;
+import net.minecraft.world.level.lighting.LevelLightEngine;
+import net.minecraft.world.level.material.FluidState;
+import org.bukkit.craftbukkit.util.BlockStateListPopulator;
+import org.jetbrains.annotations.Nullable;
+
+public class FaweBlockStateListPopulator extends BlockStateListPopulator {
+
+    private final ServerLevel world;
+
+    public FaweBlockStateListPopulator(ServerLevel world) {
+        super(world);
+        this.world = world;
+    }
+
+    @Override
+    public long getSeed() {
+        return world.getSeed();
+    }
+
+    @Override
+    public ServerLevel getLevel() {
+        return world.getLevel();
+    }
+
+    @Override
+    public MinecraftServer getServer() {
+        return world.getServer();
+    }
+
+    @Override
+    public ChunkSource getChunkSource() {
+        return world.getChunkSource();
+    }
+
+    @Override
+    public ChunkAccess getChunk(final int chunkX, final int chunkZ, final ChunkStatus leastStatus, final boolean create) {
+        return world.getChunk(chunkX, chunkZ, leastStatus, create);
+    }
+
+    @Override
+    public BiomeManager getBiomeManager() {
+        return world.getBiomeManager();
+    }
+
+    @Override
+    public Holder<Biome> getUncachedNoiseBiome(final int biomeX, final int biomeY, final int biomeZ) {
+        return world.getUncachedNoiseBiome(biomeX, biomeY, biomeZ);
+    }
+
+    @Override
+    public int getSeaLevel() {
+        return world.getSeaLevel();
+    }
+
+    @Override
+    public float getShade(final Direction direction, final boolean shaded) {
+        return world.getShade(direction, shaded);
+    }
+
+    @Override
+    public LevelLightEngine getLightEngine() {
+        return world.getLightEngine();
+    }
+
+    @Nullable
+    @Override
+    public ChunkAccess getChunkIfLoadedImmediately(final int x, final int z) {
+        return world.getChunkIfLoadedImmediately(x, z);
+    }
+
+    @Override
+    public BlockState getBlockStateIfLoaded(final BlockPos blockposition) {
+        return world.getBlockStateIfLoaded(blockposition);
+    }
+
+    @Override
+    public FluidState getFluidIfLoaded(final BlockPos blockposition) {
+        return world.getFluidIfLoaded(blockposition);
+    }
+
+    @Override
+    public WorldBorder getWorldBorder() {
+        return world.getWorldBorder();
+    }
+
+}
