623e14a5594a4378969a09e0b9975af07b01323e
SirYwell <hannesgreule@outlook.de>
2023-11-21 15:56:29 +0100

    Use spigot library feature for ZSTD

--- worldedit-bukkit/build.gradle.kts
+++ worldedit-bukkit/build.gradle.kts
@@ -161,9 +161,6 @@ tasks.named<ShadowJar>("shadowJar") {
         // If it turns out not to be true for Spigot/Paper, our only two official platforms, this can be uncommented.
         // include(dependency("org.apache.logging.log4j:log4j-api"))
         include(dependency("org.antlr:antlr4-runtime"))
-        // ZSTD does not work if relocated. https://github.com/luben/zstd-jni/issues/189 Use not latest as it can be difficult
-        // to obtain latest ZSTD lib
-        include(dependency("com.github.luben:zstd-jni:1.4.8-1"))
         relocate("org.bstats", "com.sk89q.worldedit.bstats") {
             include(dependency("org.bstats:"))
         }
