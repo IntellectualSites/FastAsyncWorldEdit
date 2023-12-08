8496ddf5b87532475ceab4664df10c11ddba9dae
Alexander Brandes <mc.cache@web.de>
2023-12-08 07:37:23 +0100

    Release 2.8.3

--- worldedit-bukkit/adapters/adapter-1_20_2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20_2/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.2-R0.1-20231125.095734-103")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.2-R0.1-20231203.034718-121")
     compileOnly(libs.paperlib)
 }
--- worldedit-bukkit/adapters/adapter-1_20_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20_4/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.3-R0.1-20231207.043048-3")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.4-R0.1-20231207.202833-1")
     compileOnly(libs.paperlib)
 }
