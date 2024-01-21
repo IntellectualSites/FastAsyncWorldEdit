41be7e38bca9c29f269d23a97ce99776be72c72e
Pierre Maurice Schwang <mail@pschwang.eu>
2024-01-21 23:03:20 +0100

    chore/ci: add renovate checks for paperweight-userdev

--- worldedit-bukkit/adapters/adapter-1_20_2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20_2/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    // url=https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/1.20.2-R0.1-SNAPSHOT
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.2-R0.1-20231203.034718-121")
     compileOnly(libs.paperlib)
 }
--- worldedit-bukkit/adapters/adapter-1_20_4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20_4/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    // url=https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/1.20.4-R0.1-SNAPSHOT
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.4-R0.1-20240106.182028-62")
     compileOnly(libs.paperlib)
 }
