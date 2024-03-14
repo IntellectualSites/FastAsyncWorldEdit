b99d22bb6252d47d37d4df42cf12826037302a06
MineFact <minefact.minecraft@gmail.com>
2024-03-14 19:26:49 +0100

    renamed UN_OFFSET to BLOCK_CENTER_OFFSET

--- worldedit-core/src/main/java/com/sk89q/worldedit/regions/shape/WorldEditExpressionEnvironment.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/regions/shape/WorldEditExpressionEnvironment.java
@@ -28,7 +28,7 @@ import com.sk89q.worldedit.math.Vector3;
 
 public class WorldEditExpressionEnvironment implements ExpressionEnvironment {
 
-    private final Vector3 UN_OFFSET = Vector3.at(0.5, 0.5, 0.5);
+    private final Vector3 BLOCK_CENTER_OFFSET = Vector3.at(0.5, 0.5, 0.5);
 
     private final Vector3 unit;
     private final Vector3 zero2;
@@ -44,7 +44,7 @@ public class WorldEditExpressionEnvironment implements ExpressionEnvironment {
     public WorldEditExpressionEnvironment(Extent extent, Vector3 unit, Vector3 zero) {
         this.extent = extent;
         this.unit = unit;
-        this.zero2 = zero.add(UN_OFFSET);
+        this.zero2 = zero.add(BLOCK_CENTER_OFFSET);
     }
 
     public BlockVector3 toWorld(double x, double y, double z) {
@@ -98,7 +98,7 @@ public class WorldEditExpressionEnvironment implements ExpressionEnvironment {
     }
 
     public WorldEditExpressionEnvironment clone() {
-        return new WorldEditExpressionEnvironment(extent, unit, zero2.subtract(UN_OFFSET));
+        return new WorldEditExpressionEnvironment(extent, unit, zero2.subtract(BLOCK_CENTER_OFFSET));
     }
     //FAWe end
 
