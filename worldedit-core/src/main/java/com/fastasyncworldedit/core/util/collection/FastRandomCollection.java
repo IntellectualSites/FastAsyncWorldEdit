d12ef8a40c26faa4243b997390391ba78577899b
Hannes Greule <SirYwell@users.noreply.github.com>
2024-03-16 20:12:21 +0100

    Allow repeated keys in RandomCollection (#2624)

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/util/collection/FastRandomCollection.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/util/collection/FastRandomCollection.java
@@ -4,15 +4,14 @@ import com.fastasyncworldedit.core.math.random.SimpleRandom;
 import com.fastasyncworldedit.core.util.MathMan;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.List;
 import java.util.Optional;
 
-public class FastRandomCollection<T> extends RandomCollection<T> {
+public final class FastRandomCollection<T> implements RandomCollection<T> {
 
     private final T[] values;
 
-    private FastRandomCollection(T[] values, SimpleRandom random) {
-        super(random);
+    private FastRandomCollection(T[] values) {
         this.values = values;
     }
 
@@ -22,16 +21,15 @@ public class FastRandomCollection<T> extends RandomCollection<T> {
      * {@code Optional} in any case.
      *
      * @param weights the weight of the values.
-     * @param random  the random generator to use for this collection.
      * @param <T>     the value type.
      * @return an {@link Optional} containing the new collection if it could be created, {@link
      *         Optional#empty()} otherwise.
      * @see RandomCollection for API usage.
      */
-    public static <T> Optional<RandomCollection<T>> create(Map<T, Double> weights, SimpleRandom random) {
+    public static <T> Optional<RandomCollection<T>> create(List<Weighted<T>> weights) {
         int max = 0;
         int[] counts = new int[weights.size()];
-        Double[] weightDoubles = weights.values().toArray(new Double[0]);
+        double[] weightDoubles = weights.stream().mapToDouble(Weighted::weight).toArray();
         for (int i = 0; i < weightDoubles.length; i++) {
             int weight = (int) (weightDoubles[i] * 100);
             counts[i] = weight;
@@ -47,21 +45,21 @@ public class FastRandomCollection<T> extends RandomCollection<T> {
             return Optional.empty();
         }
         ArrayList<T> parsed = new ArrayList<>();
-        for (Map.Entry<T, Double> entry : weights.entrySet()) {
-            int num = (int) (100 * entry.getValue());
+        for (Weighted<T> entry : weights) {
+            int num = (int) (100 * entry.weight());
             for (int j = 0; j < num / gcd; j++) {
-                parsed.add(entry.getKey());
+                parsed.add(entry.value());
             }
         }
         @SuppressWarnings("unchecked")
         T[] values = (T[]) parsed.toArray();
-        FastRandomCollection<T> fastRandomCollection = new FastRandomCollection<>(values, random);
+        FastRandomCollection<T> fastRandomCollection = new FastRandomCollection<>(values);
         return Optional.of(fastRandomCollection);
     }
 
     @Override
-    public T next(int x, int y, int z) {
-        return values[getRandom().nextInt(x, y, z, values.length)];
+    public T next(final SimpleRandom random, int x, int y, int z) {
+        return values[random.nextInt(x, y, z, values.length)];
     }
 
 }
