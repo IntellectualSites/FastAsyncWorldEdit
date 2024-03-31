2bb7298216165a55a2fad76066dcace21816d406
Alexander Brandes <mc.cache@web.de>
2024-03-31 22:47:18 +0200

    List FAWE class additions in Javadocs
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- worldedit-core/src/main/java/com/sk89q/worldedit/world/chunk/package-info.java
+++ worldedit-core/src/main/java/com/sk89q/worldedit/world/chunk/package-info.java
@@ -1,7 +1,7 @@
 /**
- * The following classes are FAWE additions:
+ * The following classes are FAWE additions
  *
- * @see com.sk89q.worldedit.world.chunk.AnvilChunk15
- * @see com.sk89q.worldedit.world.chunk.AnvilChunk17
+ * {@link com.sk89q.worldedit.world.chunk.AnvilChunk15},
+ * {@link com.sk89q.worldedit.world.chunk.AnvilChunk17}
  */
 package com.sk89q.worldedit.world.chunk;
