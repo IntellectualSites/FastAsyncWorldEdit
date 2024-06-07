93f3573caa8e0e4a593705d6c55ea707c80e2108
dordsor21 <dordsor21@gmail.com>
2024-06-07 19:14:23 +0100

    Upstream changes, use correct list of cached block changes

--- worldedit-bukkit/adapters/adapter-1_20_2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_20_R2/PaperweightFaweAdapter.java
+++ worldedit-bukkit/adapters/adapter-1_20_2/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_20_R2/PaperweightFaweAdapter.java
@@ -549,7 +549,7 @@ public final class PaperweightFaweAdapter extends FaweAdapter<net.minecraft.nbt.
         ConfiguredFeature<?, ?> configuredFeature = serverLevel
                 .registryAccess()
                 .registryOrThrow(Registries.CONFIGURED_FEATURE)
-                .get(ResourceLocation.tryParse(feature.getId()));
+                .get(ResourceLocation.tryParse(feature.id()));
         FaweBlockStateListPopulator populator = new FaweBlockStateListPopulator(serverLevel);
 
         Map<BlockPos, CraftBlockState> placed = TaskManager.taskManager().sync(() -> {
@@ -564,10 +564,7 @@ public final class PaperweightFaweAdapter extends FaweAdapter<net.minecraft.nbt.
                 )) {
                     return null;
                 }
-                return populator.getList().stream().collect(Collectors.toMap(
-                        CraftBlockState::getPosition,
-                        craftBlockState -> craftBlockState
-                ));
+                return new HashMap<>(populator.getLevel().capturedBlockStates);
             } finally {
                 serverLevel.captureBlockStates = false;
                 serverLevel.captureTreeGeneration = false;
@@ -585,7 +582,7 @@ public final class PaperweightFaweAdapter extends FaweAdapter<net.minecraft.nbt.
         Structure k = serverLevel
                 .registryAccess()
                 .registryOrThrow(Registries.STRUCTURE)
-                .get(ResourceLocation.tryParse(type.getId()));
+                .get(ResourceLocation.tryParse(type.id()));
         if (k == null) {
             return false;
         }
