ad5587ef540c637c7ed377e38b62ca5c79630ecb
dordsor21 <dordsor21@gmail.com>
2024-06-07 20:51:45 +0100

    Undo Minecraft has a lot of different methods between private and public to determine if structures can be placed. We cannot possibly cover all of them whilst also ensuring issues do not arise with generic "true"s

--- worldedit-bukkit/adapters/adapter-1_20_4/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_20_R3/PaperweightFaweAdapter.java
+++ worldedit-bukkit/adapters/adapter-1_20_4/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_20_R3/PaperweightFaweAdapter.java
@@ -583,7 +583,12 @@ public final class PaperweightFaweAdapter extends FaweAdapter<net.minecraft.nbt.
 
                     return null;
                 }
-                return new HashMap<>(populator.getLevel().capturedBlockStates);
+                Map<BlockPos, CraftBlockState> placedBlocks = populator.getList().stream().collect(Collectors.toMap(
+                        CraftBlockState::getPosition,
+                        craftBlockState -> craftBlockState
+                ));
+                placedBlocks.putAll(serverLevel.capturedBlockStates);
+                return placedBlocks;
             } finally {
                 serverLevel.captureBlockStates = false;
                 serverLevel.captureTreeGeneration = false;
@@ -627,6 +632,7 @@ public final class PaperweightFaweAdapter extends FaweAdapter<net.minecraft.nbt.
                         populator,
                         biome -> true
                 );
+
                 if (!structureStart.isValid()) {
                     return null;
                 } else {
--- worldedit-bukkit/adapters/adapter-1_21/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_21_R1/FaweBlockStateListPopulator.java
+++ worldedit-bukkit/adapters/adapter-1_21/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_21_R1/FaweBlockStateListPopulator.java
@@ -9,7 +9,6 @@ import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.BiomeManager;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.border.WorldBorder;
@@ -118,22 +117,7 @@ public class FaweBlockStateListPopulator extends BlockStateListPopulator {
 
     @Override
     public BlockState getBlockState(final BlockPos pos) {
-        BlockState state = world.getBlockState(pos);
-        try {
-            state = new BlockState(
-                    state.getBlock(),
-                    (Reference2ObjectArrayMap<Property<?>, Comparable<?>>) state.getValues(),
-                    PaperweightPlatformAdapter.getStatePropertiesCodec(state)
-            ) {
-                @Override
-                public boolean is(Block block) {
-                    return true;
-                }
-            };
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        return state;
+        return world.getBlockState(pos);
     }
 
     @Override
