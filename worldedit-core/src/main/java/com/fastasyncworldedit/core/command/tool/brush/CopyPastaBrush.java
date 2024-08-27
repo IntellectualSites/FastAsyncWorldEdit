ab5b2bcb618e4f75b8ed164725c42246599d46f1
Pierre Maurice Schwang <mail@pschwang.eu>
2024-08-27 15:23:18 +0200

    chore: move check into ResizableClipboardBuilder

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/command/tool/brush/CopyPastaBrush.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/command/tool/brush/CopyPastaBrush.java
@@ -3,7 +3,6 @@ package com.fastasyncworldedit.core.command.tool.brush;
 import com.fastasyncworldedit.core.FaweCache;
 import com.fastasyncworldedit.core.command.tool.ResettableTool;
 import com.fastasyncworldedit.core.configuration.Caption;
-import com.fastasyncworldedit.core.extent.clipboard.EmptyClipboard;
 import com.fastasyncworldedit.core.extent.clipboard.ResizableClipboardBuilder;
 import com.fastasyncworldedit.core.function.NullRegionFunction;
 import com.fastasyncworldedit.core.function.mask.AbstractDelegateMask;
@@ -88,11 +87,11 @@ public class CopyPastaBrush implements Brush, ResettableTool {
             visitor.visit(position);
             Operations.completeBlindly(visitor);
             // Build the clipboard
-            long blocks = builder.longSize();
-            Clipboard newClipboard = blocks > 0 ? builder.build() : EmptyClipboard.getInstance();
+            Clipboard newClipboard = builder.build();
             newClipboard.setOrigin(position);
             ClipboardHolder holder = new ClipboardHolder(newClipboard);
             session.setClipboard(holder);
+            long blocks = builder.longSize();
             player.print(Caption.of("fawe.worldedit.copy.command.copy", blocks));
         } else {
             AffineTransform transform = null;
