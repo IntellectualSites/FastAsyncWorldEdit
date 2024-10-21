3806a82f6f310609ef5cc3f6c77192dc1810f1dc
Hannes Greule <SirYwell@users.noreply.github.com>
2024-10-21 18:16:41 +0200

    Use data version provided by MC (#2955)

--- worldedit-bukkit/adapters/adapter-1_21/src/main/java/com/sk89q/worldedit/bukkit/adapter/ext/fawe/v1_21_R1/PaperweightAdapter.java
+++ worldedit-bukkit/adapters/adapter-1_21/src/main/java/com/sk89q/worldedit/bukkit/adapter/ext/fawe/v1_21_R1/PaperweightAdapter.java
@@ -65,6 +65,7 @@ import com.sk89q.worldedit.world.block.BlockType;
 import com.sk89q.worldedit.world.block.BlockTypes;
 import com.sk89q.worldedit.world.entity.EntityTypes;
 import com.sk89q.worldedit.world.item.ItemType;
+import net.minecraft.SharedConstants;
 import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -124,7 +125,6 @@ import org.bukkit.craftbukkit.block.data.CraftBlockData;
 import org.bukkit.craftbukkit.entity.CraftEntity;
 import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.craftbukkit.inventory.CraftItemStack;
-import org.bukkit.craftbukkit.util.CraftMagicNumbers;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.bukkit.generator.ChunkGenerator;
@@ -194,7 +194,7 @@ public final class PaperweightAdapter implements BukkitImplAdapter<net.minecraft
         // A simple test
         CraftServer.class.cast(Bukkit.getServer());
 
-        int dataVersion = CraftMagicNumbers.INSTANCE.getDataVersion();
+        int dataVersion = SharedConstants.getCurrentVersion().getDataVersion().getVersion();
         if (dataVersion != 3953 && dataVersion != 3955) {
             throw new UnsupportedClassVersionError("Not 1.21(.1)!");
         }
@@ -213,7 +213,7 @@ public final class PaperweightAdapter implements BukkitImplAdapter<net.minecraft
         );
         chunkProviderExecutorField.setAccessible(true);
 
-        this.dataFixer = new PaperweightDataConverters(CraftMagicNumbers.INSTANCE.getDataVersion(), this);
+        this.dataFixer = new PaperweightDataConverters(dataVersion, this);
 
         Watchdog watchdog;
         try {
