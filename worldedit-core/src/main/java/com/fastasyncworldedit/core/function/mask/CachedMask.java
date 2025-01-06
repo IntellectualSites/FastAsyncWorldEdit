fd9013d557f0cec226db2f12a96911a3907b51cc
SirYwell <hannesgreule@outlook.de>
2025-01-06 20:03:37 +0100

    check mask instead of extent

--- worldedit-core/src/main/java/com/fastasyncworldedit/core/function/mask/CachedMask.java
+++ worldedit-core/src/main/java/com/fastasyncworldedit/core/function/mask/CachedMask.java
@@ -85,13 +85,12 @@ public class CachedMask extends AbstractDelegateMask implements ResettableMask {
     }
 
     public boolean test(@Nullable Extent extent, BlockVector3 vector) {
-        if (!hasExtent || !(extent instanceof AbstractExtentMask)) {
+        if (!hasExtent || !(getMask() instanceof final AbstractExtentMask mask)) {
             return test(vector);
         }
         int x = vector.x();
         int y = vector.y();
         int z = vector.z();
-        AbstractExtentMask mask = (AbstractExtentMask) getMask();
         try {
             boolean check = cache_checked.add(x, y, z);
             if (!check) {
