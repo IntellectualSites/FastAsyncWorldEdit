54b98fe919265e77f520ba87af56db057c479e98
dordsor21 <dordsor21@gmail.com>
2025-02-14 10:52:22 +0000

    We should also be able to remove loadPrivately due to synchronisation changes

--- worldedit-bukkit/adapters/adapter-1_21_3/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_21_3/PaperweightGetBlocks.java
+++ worldedit-bukkit/adapters/adapter-1_21_3/src/main/java/com/sk89q/worldedit/bukkit/adapter/impl/fawe/v1_21_3/PaperweightGetBlocks.java
@@ -466,7 +466,7 @@ public class PaperweightGetBlocks extends AbstractBukkitGetBlocks<ServerLevel, L
                     }
 
                     if (createCopy) {
-                        char[] tmpLoad = loadPrivately(layerNo);
+                        char[] tmpLoad = load(layerNo);
                         char[] copyArr = new char[4096];
                         System.arraycopy(tmpLoad, 0, copyArr, 0, 4096);
                         copy.storeSection(getSectionIndex, copyArr);
@@ -528,7 +528,7 @@ public class PaperweightGetBlocks extends AbstractBukkitGetBlocks<ServerLevel, L
                                 this.reset();
                             } else if (!Arrays.equals(
                                     update(getSectionIndex, new char[4096], true),
-                                    loadPrivately(layerNo)
+                                    load(layerNo)
                             )) {
                                 this.reset(layerNo);
                         /*} else if (lock.isModified()) {
@@ -546,7 +546,7 @@ public class PaperweightGetBlocks extends AbstractBukkitGetBlocks<ServerLevel, L
 
                         newSection = PaperweightPlatformAdapter.newChunkSection(
                                 layerNo,
-                                this::loadPrivately,
+                                this::load,
                                 setArr,
                                 adapter,
                                 biomeRegistry,
