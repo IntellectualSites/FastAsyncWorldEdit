ddc969773ec7adb6e8780999f8b394aa5843db6b
Hannes Greule <SirYwell@users.noreply.github.com>
2025-06-30 17:16:46 +0200

    Update for 1.21.6 (#3211)
    * Initial changes for 1.21.6
    
    * update data version
    
    * update dev bundle
    
    * update mappings
    
    * converters
    
    * 1.21.5 dev bundle annoyances
    
    * apply sign component fix
    
    * dev bundle update

--- worldedit-bukkit/adapters/adapter-1_21_6/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_21_6/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.5-R0.1-20250618.110345-108")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.6-R0.1-20250621.050219-23")
     compileOnly(libs.paperlib)
 }
