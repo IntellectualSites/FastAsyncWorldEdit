9e23c0358bdf01f539af795f0be601e5d50d473c
Matt Bonanno <4009945+MattBDev@users.noreply.github.com>
2025-09-23 15:43:55 -0400

    Fixed a rebase issue

--- worldedit-bukkit/adapters/adapter-1_21_6/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_21_6/build.gradle.kts
@@ -7,5 +7,5 @@ plugins {
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.8-R0.1-20250829.204528-51")
-    compileOnly(libs.paperlib)
+    compileOnly(libs.paperLib)
 }
