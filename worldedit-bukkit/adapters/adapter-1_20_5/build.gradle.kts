b96313485c6df9fc1b387bef02463694bd7a5845
Matt <4009945+mattbdev@users.noreply.github.com>
2025-10-08 00:10:39 -0400

    Refactor Gradle build logic and update dependencies (#3309)
    * Add foojay to download JVMs.
    
    (cherry picked from commit cd8bca9a430aa589d4c5b0d292d5fc5ad6aa164f)
    Signed-off-by: Matt Bonanno <4009945+MattBDev@users.noreply.github.com>
    
    * Renamed some dependencies to kebab-case for clarity and to align with common Gradle practices.
    
    Signed-off-by: Matt Bonanno <4009945+MattBDev@users.noreply.github.com>
    
    * Refine build logic and dependency management in Gradle configuration.
    
    Signed-off-by: Matt Bonanno <4009945+MattBDev@users.noreply.github.com>
    
    * Update Gradle wrapper to version 9.0.0
    
    * Fix internalVersion expansion to use mapOf.
    Gradle 9 changed the behavior of this code but didn't set off any deprecation warnings...🙄
    
    * Refactor build logic to use plugins for dependency management and improve project structure.
    
    * Update tasks in build.gradle.kt to use configureEach for improved performance with lazy configuration.
    
    * Refactor dependencies in build.gradle.kts and libs.versions.toml for consistency
    
    * Add build-logic-improvements branch to GitHub Actions workflow for CI
    
    * Set maxParallelForks for tests based on available processors
    
    * Fixed a rebase issue
    
    * Update Target JVM version from 16 to 21 in library attributes
    
    * Add intellectualsites repository to allowed prefixes for better up-time guarantees
    
    * Updated gradle wrapper and foojay-resolver-convention plugin
    
    * Add PaperMC repository to adapter build logic
    
    * Refactor build logic to exclude net.kyori group from EngineHub repository and stop relocating and bundling fastutil
    
    * Reformatted buildlogic.platform.gradle.kts
    
    * Fixed artifact generation
    
    ---------
    
    Signed-off-by: Matt Bonanno <4009945+MattBDev@users.noreply.github.com>

--- worldedit-bukkit/adapters/adapter-1_20_5/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_20_5/build.gradle.kts
@@ -1,17 +1,11 @@
 import io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
 
 plugins {
-    java
-}
-
-applyPaperweightAdapterConfiguration()
-
-repositories {
-    gradlePluginPortal()
+    id("buildlogic.adapter")
 }
 
 dependencies {
     // url=https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/1.20.6-R0.1-SNAPSHOT/
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.6-R0.1-20241030.191541-126")
-    compileOnly(libs.paperlib)
+    compileOnly(libs.paperLib)
 }
