0a03dfa27903fdbfb0b78a8d7abfec8ac411cddc
Alexander Brandes <mc.cache@web.de>
2025-11-26 17:40:21 +0100

    Back to snapshot for development
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     id("xyz.jpenilla.run-paper") version "3.0.2"
 }
 
-var rootVersion by extra("2.14.2")
+var rootVersion by extra("2.14.3")
 var snapshot by extra("SNAPSHOT")
 var revision: String by extra("")
 var buildNumber by extra("")
@@ -30,7 +30,7 @@ ext {
     }
 }
 
-version = String.format("%s", rootVersion)
+version = String.format("%s-%s", rootVersion, buildNumber)
 
 if (!project.hasProperty("gitCommitHash")) {
     apply(plugin = "org.ajoberstar.grgit")
