1c61d820964da51c9a7e8fdf14c6b869e8e80e02
Alexander Brandes <mc.cache@web.de>
2025-11-26 17:28:38 +0100

    Release 2.14.2
    Signed-off-by: Alexander Brandes <mc.cache@web.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ ext {
     }
 }
 
-version = String.format("%s-%s", rootVersion, buildNumber)
+version = String.format("%s", rootVersion)
 
 if (!project.hasProperty("gitCommitHash")) {
     apply(plugin = "org.ajoberstar.grgit")
