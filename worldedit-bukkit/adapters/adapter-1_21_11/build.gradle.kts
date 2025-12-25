65192ccd5e385c8c9e440fc85d5270d9fe7f9abb
Pierre Maurice Schwang <mail@pschwang.eu>
2025-12-25 18:49:25 +0100

    feat: re-add obfuscation

--- worldedit-bukkit/adapters/adapter-1_21_11/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_21_11/build.gradle.kts
@@ -18,6 +18,6 @@ configurations {
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-R0.1-20251209.225848-3")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-R0.1-20251223.192256-16")
     compileOnly(libs.paperLib)
 }
