8357164b4bfb57ec8e39fa211d719378ff7ecc3d
Alexander Brandes <mc.cache@web.de>
2025-12-25 22:18:43 +0100

    Update lz4 to 1.10.2 (#3413)

--- worldedit-bukkit/adapters/adapter-1_21_11/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1_21_11/build.gradle.kts
@@ -4,18 +4,6 @@ plugins {
     id("buildlogic.adapter")
 }
 
-configurations {
-    all {
-        resolutionStrategy {
-            capabilitiesResolution {
-                withCapability("org.lz4:lz4-java") {
-                    selectHighestVersion()
-                }
-            }
-        }
-    }
-}
-
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
     the<PaperweightUserDependenciesExtension>().paperDevBundle("1.21.11-R0.1-20251223.192256-16")
