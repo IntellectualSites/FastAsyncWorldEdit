94a3b5098eb6e7cd38fdd7bc7eee170856beed7e
evlad <emmanuelvlad@gmail.com>
2026-04-08 02:16:20 +0300

    remove toolchain comment

--- worldedit-bukkit/adapters/adapter-26_1/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-26_1/build.gradle.kts
@@ -3,8 +3,6 @@ import io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
 plugins {
     id("buildlogic.adapter")
 }
-//
-//java.toolchain.languageVersion.set(JavaLanguageVersion.of(25))
 
 dependencies {
     // https://artifactory.papermc.io/ui/native/universe/io/papermc/paper/dev-bundle/
