612b51d5a4cc2416fd96cf4d75bc6b1d1cfd9185
SirYwell <hannesgreule@outlook.de>
2026-07-04 14:41:12 +0200

    leafpile snakeyaml workaround (??)

--- worldedit-bukkit/adapters/adapter-26.2/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-26.2/build.gradle.kts
@@ -4,8 +4,16 @@ plugins {
     id("buildlogic.adapter")
 }
 
+// workaround LeafPile providing wrong snakeyaml version
+configurations.named("testRuntimeClasspath") {
+    exclude(group = "org.yaml", module = "snakeyaml")
+}
+
 dependencies {
     // https://artifactory.papermc.io/ui/native/universe/io/papermc/paper/dev-bundle/
     the<PaperweightUserDependenciesExtension>().paperDevBundle("26.2.build.+")
     compileOnly(libs.paperLib)
+    testRuntimeOnly(libs.snakeyaml) {
+        because("Keep adapter tests on Bukkit's SnakeYaml while excluding LeafPile's conflicting transitive version")
+    }
 }
